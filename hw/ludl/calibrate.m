470b7828642011ea330210a47fdc91eb208ba610
Megan Pramojaney <pramoj23@live.unc.edu>
2019-12-10 12:02:18 -0500

    Added length and width

--- hw/ludl/calibrate.m
+++ hw/ludl/calibrate.m
@@ -12,19 +12,19 @@ for im = 1:size(points,3)
     centers(im,2) = y;
 end
 
-% % Creat error matrix of side lengths, order sides as NSEW, with top length as "North"
-% length = 1000;
-% width = 1000;
-% errormatrix = zeros(4,2);
-% 
-% % Finds error in tick marks (col 1)
-% errormatrix(1,1) = abs(centers(1:1)-centers(2:1)) - length;
-% errormatrix(2,1) = abs(centers(4:1)-centers(3:1)) - length;
-% errormatrix(3,1) = abs(center(2:2)-centers(3:2)) - width;
-% errormatrix(4,1) = abs(centers(1:2)-centers(4:2)) - width;
-% 
-% % Finds percent error (col 2)
-% errormatrix(1,2) = errormatrix(1,1)/length * 100;
-% errormatrix(2,2) = errormatrix(2,1)/length * 100;
-% errormatrix(3,2) = errormatrix(3,1)/width * 100;
-% errormatrix(4,2) = errormatrix(4,1)/width * 100;
+% Creat error matrix of side lengths, order sides as NSEW, with top length as "North"
+length = mm_to_tick(69.6);
+width = mm_to_tick(44.476);
+errormatrix = zeros(4,2);
+
+% Finds error in tick marks (col 1)
+errormatrix(1,1) = abs(centers(1:1)-centers(2:1)) - length;
+errormatrix(2,1) = abs(centers(4:1)-centers(3:1)) - length;
+errormatrix(3,1) = abs(center(2:2)-centers(3:2)) - width;
+errormatrix(4,1) = abs(centers(1:2)-centers(4:2)) - width;
+ 
+% Finds percent error (col 2)
+errormatrix(1,2) = errormatrix(1,1)/length * 100;
+errormatrix(2,2) = errormatrix(2,1)/length * 100;
+errormatrix(3,2) = errormatrix(3,1)/width * 100;
+errormatrix(4,2) = errormatrix(4,1)/width * 100;
