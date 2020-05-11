77f207c09028599a8fd43bd03c84fe701d010d4c
Jeremy Cribb <cribb@cs.unc.edu>
2020-05-11 14:00:31 -0400

    New function that outputs a vector of wells in the order the video data should be collected.

---
+++ automation/ba_well_visitorder.m
@@ -0,0 +1,29 @@
+function vo = ba_well_visitorder(platedef, sampling)
+% BA_WELL_VISITORDER outputs the order in which to visit wells in the Adhesion Assay
+%
+% vo = ba_well_visitorder(platedef, sampling)
+%
+%
+% Inputs:
+% Outputs: 
+
+if nargin < 2 || isempty(sampling)
+    sampling = 'sorted';
+end
+
+    vidx = ~cellfun('isempty', platedef.Value);
+    well_list = unique(platedef.Well_ID(vidx));
+    N = length(well_list);
+    
+    switch sampling
+        case 'sorted'
+            vo = sort(well_list);
+        case 'random'
+            new_order = randsample(N, N);
+            vo = well_list(new_order);
+        otherwise
+            error('Sampling order not recognized.');
+    end
+    
+    
+    
