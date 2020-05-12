608abcc14c6d3d4e881ea8c723e7862281a2e7c6
Jeremy Cribb <cribb@cs.unc.edu>
2020-05-12 16:49:10 -0400

    Tweaked variable names for readability and added calibration structure as member of plate.

--- automation/ba_calibrate_plate.m
+++ automation/ba_calibrate_plate.m
@@ -1,36 +1,47 @@
-function [centers, theta, errormatrix] = ba_calibrate_plate(ludl)
+function outs = ba_calibrate_plate(ludl)
 % BA_CALIBRATE_PLATE uses the fiducial marking images taken by the camera and the
 % location of the stage in ludl coordinates at that location to calculate
 % the location of the sample on the ludl stage
 
-% Finds fiducial positions
-centers = zeros(4,2);
-[pos, points] = fiducial_position_array(ludl);
+
+plate_length_mm = 69.6;
+plate_width_mm = 44.476;
+plate_length_ticks = mm2tick(plate_length_mm);
+plate_width_ticks = mm2tick(plate_width_mm);
+
+% locate the fiducial marks, then stores the images and the positions in 
+% ludl coordinates at which each image was taken.
+[pos, imstack] = fiducial_position_array(ludl);
 
 % Finds the ludl coordinates that would place the fiducial center in the
 % center of the image
-for im = 1:size(points,3)
-    [x, y] = image_center_find(points(:,:,im), pos(im,1), pos(im,2));
-    centers(im,1) = x;
-    centers(im,2) = y;
+for im = 1:size(imstack,3)
+    [x,y] = image_center_find(imstack(:,:,im), pos(im,1), pos(im,2));
+    outs.centers(im,1) = x;
+    outs.centers(im,2) = y;
 end
 
 % Find the angle of tilt in radians
-theta = atan(abs(centers(2,1)-centers(1,1))/abs(centers(2,2)-centers(1,2)));
+Ftop_leftXY = outs.centers(1,:);
+Ftop_rightXY = outs.centers(2,:);
+Fbottom_rightXY = outs.centers(3,:);
+Fbottom_leftXY = outs.centers(4,:);
+
+outs.theta = atan(abs(Ftop_rightXY(1) - Ftop_leftXY(1))/abs(Ftop_rightXY(2) - Ftop_leftXY(2)));
 
 % Creat error matrix of side lengths, order sides as NSEW, with top length as "North"
-length = mm2tick(69.6);
-width = mm2tick(44.476);
 errormatrix = zeros(4,2);
 
 % Finds error in tick marks (col 1)
-errormatrix(1,1) = pdist([centers(1,1:2);centers(2,1:2)]) - length;
-errormatrix(2,1) = pdist([centers(3,1:2);centers(4,1:2)]) - length;
-errormatrix(3,1) = pdist([centers(2,1:2);centers(3,1:2)]) - width;
-errormatrix(4,1) = pdist([centers(1,1:2);centers(4,1:2)]) - width;
- 
+errormatrix(1,1) = pdist([Ftop_leftXY;Ftop_rightXY]) - plate_length_ticks;
+errormatrix(2,1) = pdist([Fbottom_rightXY;Fbottom_leftXY]) - plate_length_ticks;
+errormatrix(3,1) = pdist([Ftop_rightXY;Fbottom_rightXY]) - plate_width_ticks;
+errormatrix(4,1) = pdist([Ftop_leftXY;Fbottom_leftXY]) - plate_width_ticks;
+
 % Finds percent error (col 2)
-errormatrix(1,2) = errormatrix(1,1)/length * 100;
-errormatrix(2,2) = errormatrix(2,1)/length * 100;
-errormatrix(3,2) = errormatrix(3,1)/width * 100;
-errormatrix(4,2) = errormatrix(4,1)/width * 100;
+errormatrix(1,2) = errormatrix(1,1)/plate_length_ticks * 100;
+errormatrix(2,2) = errormatrix(2,1)/plate_length_ticks * 100;
+errormatrix(3,2) = errormatrix(3,1)/plate_width_ticks * 100;
+errormatrix(4,2) = errormatrix(4,1)/plate_width_ticks * 100;
+
+outs.errormatrix = errormatrix;
