1c2e99289abe800aaedf635de2325cd5d6a2c9dd
Jeremy Cribb <cribb@cs.unc.edu>
2020-05-12 15:11:28 -0400

    Cleaned code via Matlab lint-checker.

--- hw/ludl/image_center_find.m
+++ hw/ludl/image_center_find.m
@@ -1,4 +1,4 @@
-function [x_center y_center x_disp y_disp] = image_center_find(test, x_start, y_start)
+function [x_center, y_center, x_disp, y_disp] = image_center_find(test, x_start, y_start)
 % IMAGE_CENTER_FIND locates the center of each fiducial mark and determines 
 % the ludl coordinates that the stage would need to be in such that the 
 % center of each fiducial mark would be at the center of the image
@@ -15,12 +15,13 @@ test = imdilate(test,SE);
 test = imerode(test,SE); imshow(test)
 
 % Find/plot center of mass
-[x y sx sy] = centerofmass(test);
+[x, y] = centerofmass(test);
 hold on
-plot(x,y,'or');
+plot(x, y, 'or');
+hold off
 
 % Find pixel displacement from center and convert to ticks
-x_disp = (635/2 - x) * - factor;
+x_disp = (635/2 - x) * -factor;
 y_disp = (470/2 - y) * factor;
 
 % Find coordinates for the center of the fiducial markings in ludl
