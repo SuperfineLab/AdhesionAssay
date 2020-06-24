b8922cc6bfdb2cc30d6579b06825e3b01aa230ee
Megan Pramojaney <pramoj23@live.unc.edu>
2020-06-24 14:34:12 -0400

    Added second version case for plate design

--- automation/plate_space_move.m
+++ automation/plate_space_move.m
@@ -1,4 +1,4 @@
-function plate_space_move(ludl, plate, wellcor, movegrid_plate)
+function plate_space_move(ludl, plate, wellcor, movegrid_plate, platelayout)
 % PLATE_SPACE_MOVE moves to the center of a given well
 
 % centers - location of center of fiducial marks
@@ -9,6 +9,22 @@ function plate_space_move(ludl, plate, wellcor, movegrid_plate)
 % movegrid - an optional parameter which indicates where the view should
 % be moved relative to the center of the well in units of millimeters
 
+%% Selecting Well-Layout
+
+% Note: For distances x and y are reversed and both negative when written in 
+% Ludl space. Distances are in units of mm.
+
+switch platelayout
+    case 1  % Phase 1 case
+        well_one_cent = [6.037 11.216];
+        interwell_dist = [16.2 11.79];
+    case 2  % Phase 2 case
+        well_one_cent = [4.24 7.8];
+        interwell_dist = [9.0 9.0];
+end
+
+%% Determining Distances
+
 centers = plate.calib.centers;
 theta = plate.calib.theta;
 
@@ -18,20 +34,25 @@ origin = centers(1,:);
 % Defining distances in mm
 % Note: x and y are reversed and both negative -> written in Ludl
 % coordinates
-well_one_cent_x = 6.037;
-well_one_cent_y = 11.216;
-interwell_dist_x = 16.2;
-interwell_dist_y = 11.79; 
+% well_one_cent_x = 6.037;
+% well_one_cent_y = 11.216;
+% interwell_dist_x = 16.2;
+% interwell_dist_y = 11.79; 
 
 % Convert mm to ticks
-well_one_x_tick = mm2tick(well_one_cent_x);
-well_one_y_tick = mm2tick(well_one_cent_y);
-interwell_x_tick = mm2tick(interwell_dist_x);
-interwell_y_tick = mm2tick(interwell_dist_y);
+% well_one_x_tick = mm2tick(well_one_cent_x);
+% well_one_y_tick = mm2tick(well_one_cent_y);
+% interwell_x_tick = mm2tick(interwell_dist_x);
+% interwell_y_tick = mm2tick(interwell_dist_y);
+well_one_tick = mm2tick(well_one_cent);
+interwell_tick = mm2tick(interwell_dist);
 
 % Find moving distance
-dist_x = -(well_one_x_tick + (wellcor(1) - 1) * interwell_x_tick);
-dist_y = -(well_one_y_tick + (wellcor(2) - 1) * interwell_y_tick);
+% dist_x = -(well_one_x_tick + (wellcor(1) - 1) * interwell_x_tick);
+% dist_y = -(well_one_y_tick + (wellcor(2) - 1) * interwell_y_tick);
+dist_x = -(well_one_tick(1) + (wellcor(1) - 1) * interwell_tick(1));
+dist_y = -(well_one_tick(2) + (wellcor(2) - 1) * interwell_tick(2));
+
 
 % Correction factor in x direction
 
