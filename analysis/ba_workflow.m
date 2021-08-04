4ee4ae043c3e3f0395734432e7b2e7a958e4e800
Jeremy Cribb <cribb@cs.unc.edu>
2021-08-04 15:37:20 -0400

    Turning on and off blocks of code by commenting again :(

--- analysis/ba_workflow.m
+++ analysis/ba_workflow.m
@@ -58,18 +58,18 @@ for s = 1:S
 
 
 %     logentry('Loading frame extraction times and motor z-positions');
-    tz = load([stackdir '.meta.mat']);
+%     tz = load([stackdir '.meta.mat']);
     
 %     logentry('Retrieving first & last frames (used for first locating beads).');
 %     ba_extract_keyframes(stackdir);
-    
-    logentry('Creating mini-video of stack using MP4 format');
-    ba_minivideo(stackdir);
-    
+%     
+%     logentry('Creating mini-video of stack using MP4 format');
+%     ba_minivideo(stackdir);
+%     
 %     logentry('Tracking beads in this stack...');
-%     ba_trackstack(stackdir);
+    ba_trackstack(stackdir);
 
-% 
+
 %     logentry('Compressing stack into smaller zip file.');
 %     ba_zipstack(stackdir);
     
