d3dcd90086907d0dc4f852f811069003c33d01f3
Jeremy Cribb <cribb@cs.unc.edu>
2021-08-04 15:47:24 -0400

    Updated to use new camera functions and now doesn't write over a dataset/stack that's already been collected.

--- calib/ba_collect_psf.m
+++ calib/ba_collect_psf.m
@@ -1,31 +1,10 @@
-function ba_collect_psf(outputFileName, exptime)
+function outs = ba_collect_psf(zhand, outputStackDir, exptime)
 % BA_COLLECT_PSF drives the hardware when collecting point-spread functions
 % 
 
-% Make h a global variable so it can be used outside the main
-% function. Useful when you do event handling and sequential           
-global h; 
-
-% Initialize GUI for motor control
-fpos=get(0,'DefaultFigurePosition');
-fpos(3)=650; % Figure width
-fpos(4)= 450;% Figure height
-f = figure('Position', fpos,'Menu','None','Name','APT GUI');
-
-% Create ActiveX Controller
-% !regsvr64 /s C:\Program Files (x86)\Thorlabs\APT\APT Server\MG17Motor.ocx
-h = actxcontrol('MGMOTOR.MGMotorCtrl.1',[20 20 600 400], f);
-h.StartCtrl;
-SN = 83829797; %Set Serial number
-set(h,'HWSerialNum', SN);
-h.Identify; 
-pause(5); 
-
-% Start camera
-vid = videoinput('pointgrey', 1,'F7_Raw16_1024x768_Mode2');
-vid.ReturnedColorspace = 'grayscale';
-
-myprops.vid = propinfo(vid);
+if nargin < 1 || isempty(zhand) || ~contains(class(zhand), 'COM.MGMOTOR_MGMotorCtrl_1')
+    error('Handle to the z-motor was incorrect type or not provided. Type "help tm_initz" for details.');    
+end
 
 % This only works if all of the modes are set before Matlab gets ahold of
 % the camera object. To make sure this is the case, run FLIR's (Point
@@ -34,63 +13,92 @@ myprops.vid = propinfo(vid);
 % particular properties is merely a "formality" until a bug is fixed in
 % Matlab that doesn't allow changes to these modes to persist after setting
 % them.
-src = getselectedsource(vid); 
-src.ExposureMode = 'off'; 
-src.FrameRateMode = 'off';
-src.ShutterMode = 'manual';
-src.Gain = 10;
-src.Gamma=1.15;
-src.Brightness=5.8594;
+
+% Pull the current working directory
+rootdir = pwd;
+imageType = 'pgm';
+
+% Check for existence of stackdir. We don't want to overwrite any previous
+% data.
+if ~isempty(dir(outputStackDir))
+    error('Stack Directory exists already. Choose another name to avoid overwriting dats.');
+end
+
+mkdir(outputStackDir);
+cd(outputStackDir);
+
+%
+% % Controlling the Hardware
+%
+
+% Camera Setup
+CameraName = 'Grasshopper3';
+CameraFormat = 'F7_Raw16_1024x768_Mode2';
+ExposureTime = exptime;
+Video = flir_config_video(CameraName, CameraFormat, ExposureTime);
+[cam, src] = flir_camera_open(Video);
+vidRes = cam.VideoResolution;
+imageRes = fliplr(vidRes);
 pause(0.1);
 
-
-% myprops.shutter = propinfo(src,'Shutter')
-pause(0.1);
-
-src.Shutter = exptime;
-
-myprops.shutterpost = propinfo(src,'Shutter');
-trigconf = triggerconfig(vid, 'manual');
-vid.FramesPerTrigger = Inf;
-% preview(vid);
+% trigconf = triggerconfig(cam, 'manual');
+triggerconfig(cam, 'manual');
+cam.FramesPerTrigger = Inf;
+% preview(cam);
 pause(.2)
-
-% Controlling the Hardware
+outs = struct;
 
 % Initialize vector for height and time at each frame
 nSteps = 300;
 stepsize = 0.001; % [mm]
-ztvector = zeros(nSteps,2); 
-imgstack = uint16(zeros(768,1024,nSteps));
-height = 0;
 
 % Set method/mode for jogging instead of continuous motion
-h.SetJogStepSize(0,stepsize);
-h.SetJogVelParams(0,1,5,2);
-max_intensity = NaN(300,1);
+zhand.SetJogStepSize(0,stepsize);
+zhand.SetJogVelParams(0,1,5,2);
+
+% Pre-fill the outputs
+ztvector = zeros(nSteps,1); 
+max_intensity = NaN(nSteps,1);
+imgstack = cell(nSteps,1);
+
+% imgstack = uint16(zeros(768,1024,nSteps));
+height = 0;
+
 
 % Loop occurs until motor position reaches 0
-for i = 1:nSteps
-    %take snapshot and save image matrix into imgstack
-    [pic, data]=getsnapshot(vid); 
+for k = 1:nSteps
     
-    max_intensity(i,1) = max(pic(:));
-    fprintf('pos = %i, max_intensity = %i \n', i, max_intensity(i,1));
-    imgstack(:,:,i) = pic;
+    %take snapshot and save image matrix into imgstack
+    [pic, metadata(k)] = getsnapshot(cam); 
+    
+    % Saving images as a stack 
+    filestring = ['psfHeight-' num2str(k, '%04i') '.' imageType];
+    outfile = fullfile(rootdir, outputStackDir, filestring);
+    imwrite(pic, outfile, imageType);
+    
+    % Add image to cell array for table output
+    imgstack{k} = pic;
     
     % Get position of motor and store in ztvector
-    height = h.GetPosition_Position(0); 
-    ztvector(i,1) = height;
-       
+    height = tm_getz(zhand); 
+    ztvector(k,1) = height;
+
+    % Calculate and share image's maximum intensity
+    max_intensity(k,1) = max(pic(:));
+    fprintf('pos = %i, max_intensity = %i \n', k, max_intensity(k,1));
+    
     % Jog the position of the motor by one step (up)
-    h.MoveJog(0,1); %1 raises magnet, 2 lowers magnet    
+    zhand.MoveJog(0,1); %1 raises magnet, 2 lowers magnet    
     pause(1);
     
-    % Saving data as a TIF stack 
-    imwrite(imgstack(:, :, i), outputFileName, 'WriteMode', 'append');
-
 end
-xlswrite([outputFileName '.xlsx'],ztvector)
+
+cd(rootdir);
+
+outs = table(ztvector, max_intensity, imgstack, ...
+       'VariableNames', {'ZposAboveSurface', 'MaxIntensity', 'BeadImage'});
+outs.Properties.VariableUnits = {'[mm]','',''};
+% xlswrite([outputFileName '.xlsx'],ztvector)
 
 
 
