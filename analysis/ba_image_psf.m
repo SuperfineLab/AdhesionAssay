94b6770652f86068bd61b3fd8967abd4b270f114
Jeremy Cribb <cribb@cs.unc.edu>
2021-10-21 11:53:37 -0400

    New function for imaging spread functions for beads.

---
+++ analysis/ba_image_psf.m
@@ -0,0 +1,13 @@
+function image_my_psf(stim, gaussSD, isovalue)
+figure;
+stFilt = imgaussfilt3(stim,gaussSD);
+stFilt = double(stFilt);
+stFilt = stFilt ./ max(stFilt(:));
+p = patch(isosurface(stFilt,isovalue));
+isonormals(stFilt,p);
+p.FaceColor = 'red';
+p.EdgeColor = 'none';
+daspect(1./[0.692 0.692 5])
+axis tight
+camlight
+lighting gouraud
