fe07851e0668c3c03381413fb69c8a68f8e70d7c
Jeremy Cribb <cribb@cs.unc.edu>
2022-07-20 17:05:02 -0400

    Corrected the interwell distances. X and Y were swapped from what they should be.

--- config/platedef.m
+++ config/platedef.m
@@ -12,7 +12,8 @@ switch platelayout
             plate.length_mm = 57.96;
             plate.width_mm = 43.2;
             plate.well_one_center = [5.4, 5.4];
-            plate.interwell_dist  = [16.2, 11.79];
+%             plate.interwell_dist  = [16.2, 11.79];
+            plate.interwell_dist  = [11.79, 16.2];
         case 'nunc-96' % nunc spec.
             error('nunc-96 is not yet defined.');
 %         plate.well_one_center = [14.32 11.25];
