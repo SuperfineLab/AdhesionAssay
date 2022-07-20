d931410e82c943bf108ae09390b910f6d7f07a2e
Jeremy Cribb <cribb@cs.unc.edu>
2022-07-20 08:30:11 -0400

    Refactoring code for the calibration routine. Planning functions for transforming between coordinate spaces.

--- automation/ba_calibrate_plate.m
+++ automation/ba_calibrate_plate.m
@@ -1,4 +1,4 @@
-function outs = ba_calibrate_plate(ludl, platelayout)
+function cal = ba_calibrate_plate(ludl, platelayout)
 % BA_CALIBRATE_PLATE uses the fiducial marking images taken by the camera and the
 % location of the stage in ludl coordinates at that location to calculate
 % the location of the sample on the ludl stage
@@ -7,162 +7,71 @@ if nargin < 2 || isempty(platelayout)
     platelayout = '15v2';
 end
 
-switch platelayout
-    case '15v1'
-        % 15 well plate version 1 (meganp)
-        plate_length_mm = 69.6;
-        plate_width_mm = 44.476;
-    case '15v2'
-        % 15 well plate version 2 (wollensack)
-        plate_length_mm = 57.96;
-        plate_width_mm = 43.2;
-    otherwise
-        error('Unknown plate layout defined.');
+plate = platedef(platelayout);
+
+cameraname = 'Grasshopper3';
+switch lower(cameraname)
+    case 'grasshopper3'
+        imSpec.Width = 1024;
+        imSpec.Height = 768;
+        imSpec.SE_radius = 22;
+    case 'dragonfly2'
+        imSpec.Width = 648;
+        imSpec.Height = 488;
+        imSpec.SE_radius = 14;        
 end
 
-plate_length_ticks = mm2tick(ludl, plate_length_mm);
-plate_width_ticks = mm2tick(ludl, plate_width_mm);
+plate.length_ticks = mm2tick(ludl, plate.length_mm);
+plate.width_ticks = mm2tick(ludl, plate.width_mm);
+
+% check for open impreview window
+h = findobj('Name', 'vid_impreview');
+if ~isempty(h)
+    logentry('Found open preview window. Closing.')
+    close(h);
+    pause(0.1);
+end
+
 
 % locate the fiducial marks, then stores the images and the positions in 
 % ludl coordinates at which each image was taken.
-[pos, imstack] = fiducial_position_array(ludl, platelayout);
+[pos, imstack] = fiducial_position_array(ludl, plate, imSpec);
 
 % Finds the ludl coordinates that would place the fiducial center in the
 % center of the image
-for k = 1:size(imstack,3)
-    [x,y] = image_center_find(imstack(:,:,k), pos(k,1), pos(k,2));
-    outs.centers(k,1) = x;
-    outs.centers(k,2) = y;
-end
+[x,y] = image_center_find(imstack, pos, imSpec);
+cal.centers(:,1) = x;
+cal.centers(:,2) = y;
 
-% Find the angle of tilt in radians
-Ftop_leftXY = outs.centers(1,:);
-Ftop_rightXY = outs.centers(2,:);
-Fbottom_rightXY = outs.centers(3,:);
-Fbottom_leftXY = outs.centers(4,:);
 
-outs.theta = atan(abs(Ftop_rightXY(1) - Ftop_leftXY(1))/abs(Ftop_rightXY(2) - Ftop_leftXY(2)));
+cal.theta = calculate_tilt_angle(cal);
 
-% Create error matrix of side lengths, order sides as NSEW, with top length as "North"
-errormatrix = zeros(4,2);
+cal.errormatrix = calculate_error_matrix(cal, plate);
 
-% Finds error in tick marks (col 1)
-errormatrix(1,1) = pdist([Ftop_leftXY;Ftop_rightXY]) - plate_length_ticks;
-errormatrix(2,1) = pdist([Fbottom_rightXY;Fbottom_leftXY]) - plate_length_ticks;
-errormatrix(3,1) = pdist([Ftop_rightXY;Fbottom_rightXY]) - plate_width_ticks;
-errormatrix(4,1) = pdist([Ftop_leftXY;Fbottom_leftXY]) - plate_width_ticks;
-
-% Finds percent error (col 2)
-errormatrix(1,2) = errormatrix(1,1)/plate_length_ticks * 100;
-errormatrix(2,2) = errormatrix(2,1)/plate_length_ticks * 100;
-errormatrix(3,2) = errormatrix(3,1)/plate_width_ticks * 100;
-errormatrix(4,2) = errormatrix(4,1)/plate_width_ticks * 100;
-
-outs.errormatrix = errormatrix;
+% If we had a preview, restore it once we're done collecting data
+% if ~isempty(h)
+%     ba_impreview(hw, viewOps);
+% end
 
 return
 
 
-
-function [x_center, y_center, x_disp, y_disp] = image_center_find(im, x_start, y_start)
-% IMAGE_CENTER_FIND locates the center of each fiducial mark and determines 
-% the ludl coordinates that the stage would need to be in such that the 
-% center of each fiducial mark would be at the center of the image
-
-    cameraname = 'Grasshopper3';
-    switch lower(cameraname)
-        case 'grasshopper3'
-            image_width_pixels = 1024;
-            image_height_pixels = 768;
-            SE_radius = 22;
-        case 'dragonfly2'
-            image_width_pixels = 648;
-            image_height_pixels = 488;
-            SE_radius = 14;        
-    end
-
-    % Define structuring element
-    SE = strel('disk', SE_radius);
-    factor = 45;
-
-    % Binarize and erode/dilate image
-    test = im;
-    test = imbinarize(test);
-    
-    if sum(test(:)) > 1/2 * (image_width_pixels * image_height_pixels)       
-        test = imcomplement(test); 
-    end
-    
-    test = imdilate(test,SE); 
-    test = imerode(test,SE); 
-
-    % (10:end-10,10:end-10)
-    % Find/plot center of mass
-    [x, y] = centerofmass(test);
-
-    % Find pixel displacement from center and convert to ticks
-    % x_disp = (635/2 - x) * -factor;
-    % y_disp = (470/2 - y) * factor;
-
-    x_disp = (image_width_pixels/2 - x) * -factor;
-    y_disp = (image_height_pixels/2 - y) * factor;
-
-    % Find coordinates for the center of the fiducial markings in ludl
-    % coordinates
-    x_center = x_start + x_disp;
-    y_center = y_start + y_disp;
-
-    % Debug plot (comment out later)
-    figure; 
-    imshow(test)
-    hold on
-        plot(x, y, 'or');
-        plot(x_center, y_center, 'xg');   
-    hold off
-    legend('center of mass', 'center of field');
-
-return
-
-
-function [FidLudlLocs, imstack] = fiducial_position_array(ludl, platelayout)
+function [FidLudlLocs, imstack] = fiducial_position_array(ludl, plate, imSpec)
 % FIDUCIAL_POSITION_ARRAY opens a viewing window for the user to manually
 % locate the fiducial marks, then stores the images and the positions in 
 % ludl coordinates at which each image was taken.
 
-    if nargin < 2 || isempty(platelayout)
-        platelayout = '15v2';
-    end
     
-    % image_width_pixels = 648;
-    % image_height_pixels = 488;
-
-    image_width_pixels = 1024;
-    image_height_pixels = 768;
-
-    switch platelayout
-        case '15v1'
-            % Distances between fiducial marks
-            % Version 1 (meganp)
-            plate_length_mm = 69.6;
-            plate_width_mm = 44.476;
-        case '15v2'
-            % Version 2 (david wollensak)
-            plate_length_mm = 57.96;
-            plate_width_mm = 43.2;
-        otherwise
-            error('Unknown platelayout.');
-    end
-    
-    plate_length_ticks = mm2tick(ludl, plate_length_mm);
-    plate_width_ticks = mm2tick(ludl, plate_width_mm);
+    plate.length_ticks = mm2tick(ludl, plate.length_mm);
+    plate.width_ticks = mm2tick(ludl, plate.width_mm);
 
     % WARNING: Adjust the size of imstack according to image size!
-    imstack = zeros(image_height_pixels, image_width_pixels, 4);
+    imstack = zeros(imSpec.Height, imSpec.Width, 4);
 
     FiducialOffsets = [                  0,                   0 ; ...
-                                         0, -plate_length_ticks ; ...
-                        -plate_width_ticks, -plate_length_ticks ; ...
-                        -plate_width_ticks,                   0 ];
+                                         0, -plate.length_ticks ; ...
+                        -plate.width_ticks, -plate.length_ticks ; ...
+                        -plate.width_ticks,                   0 ];
     FidLudlLocs = zeros(4,2);
 
 
@@ -220,3 +129,95 @@ function [FidLudlLocs, imstack] = fiducial_position_array(ludl, platelayout)
     end
 
 return
+
+
+function [x_center, y_center, x_disp, y_disp] = image_center_find(imstack, xypos, imSpec)
+% IMAGE_CENTER_FIND locates the center of each fiducial mark and determines 
+% the ludl coordinates that the stage would need to be in such that the 
+% center of each fiducial mark would be at the center of the image
+
+    % Define structuring element
+    SE = strel('disk', imSpec.SE_radius);
+    factor = 45;
+
+    Nframes = size(imstack,3);
+    x_center = NaN(Nframes,1); y_center = NaN(Nframes,1); 
+    for k = 1:Nframes
+        
+        % Binarize and erode/dilate image
+        test = imstack(:,:,k);
+        test = imbinarize(test);
+
+        if sum(test(:)) > 1/2 * (imSpec.Width * imSpec.Height)       
+            test = imcomplement(test); 
+        end
+
+        test = imdilate(test,SE); 
+        test = imerode(test,SE); 
+
+        % (10:end-10,10:end-10)
+        % Find/plot center of mass
+        [x, y] = centerofmass(test);
+
+        % Find pixel displacement from center and convert to ticks
+        % x_disp = (635/2 - x) * -factor;
+        % y_disp = (470/2 - y) * factor;
+
+        x_disp = (imSpec.Width/2 - x) * -factor;
+        y_disp = (imSpec.Height/2 - y) * factor;
+
+        % Find coordinates for the center of the fiducial markings in ludl
+        % coordinates
+        x_center(k,1) = xypos(k,1) + x_disp;
+        y_center(k,1) = xypos(k,2) + y_disp;
+
+%         % Debug plot (comment out later)
+%         figure; 
+%         imshow(test)
+%         hold on
+%             plot(x, y, 'or');
+%             plot(x_center, y_center, 'xg');   
+%         hold off
+%         legend('center of mass', 'center of field');
+    end
+
+return
+
+
+function theta = calculate_tilt_angle(cal)
+    % Find the angle of tilt in radians
+    Ftop_leftXY = cal.centers(1,:);
+    Ftop_rightXY = cal.centers(2,:);
+    Fbottom_rightXY = cal.centers(3,:);
+    Fbottom_leftXY = cal.centers(4,:);
+
+    theta = atan(abs(Ftop_rightXY(1) - Ftop_leftXY(1))/abs(Ftop_rightXY(2) - Ftop_leftXY(2)));
+return
+
+
+function errormatrix = calculate_error_matrix(cal, plate)
+    
+    Ftop_leftXY = cal.centers(1,:);
+    Ftop_rightXY = cal.centers(2,:);
+    Fbottom_rightXY = cal.centers(3,:);
+    Fbottom_leftXY = cal.centers(4,:);
+
+    cal.theta = atan(abs(Ftop_rightXY(1) - Ftop_leftXY(1))/abs(Ftop_rightXY(2) - Ftop_leftXY(2)));
+
+    % Create error matrix of side lengths, order sides as NSEW, with top length as "North"
+    errormatrix = zeros(4,2);
+
+    % Finds error in tick marks (col 1)
+    errormatrix(1,1) = pdist([Ftop_leftXY;Ftop_rightXY]) - plate.length_ticks;
+    errormatrix(2,1) = pdist([Fbottom_rightXY;Fbottom_leftXY]) - plate.length_ticks;
+    errormatrix(3,1) = pdist([Ftop_rightXY;Fbottom_rightXY]) - plate.width_ticks;
+    errormatrix(4,1) = pdist([Ftop_leftXY;Fbottom_leftXY]) - plate.width_ticks;
+
+    % Finds percent error (col 2)
+    errormatrix(1,2) = errormatrix(1,1)/plate.length_ticks * 100;
+    errormatrix(2,2) = errormatrix(2,1)/plate.length_ticks * 100;
+    errormatrix(3,2) = errormatrix(3,1)/plate.width_ticks * 100;
+    errormatrix(4,2) = errormatrix(4,1)/plate.width_ticks * 100;
+
+return
+
