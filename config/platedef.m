b4d9c43290ebe38a12956217251af4feb65dee34
Jeremy Cribb <cribb@cs.unc.edu>
2022-07-20 08:31:59 -0400

    Added plate name to config output. Starting to make this more generic in anticipation of 12-well plates used in phagocytosis assay.

--- config/platedef.m
+++ config/platedef.m
@@ -1,8 +1,9 @@
 function plate = platedef(platelayout)
 
+plate.name = platelayout;
+
 switch platelayout
-        case '15v1' % nunc spec.
-    %         plate.well_one_center = [14.32 11.25];
+        case '15v1' 
             plate.length_mm = 69.6;
             plate.width_mm = 44.476;
             plate.well_one_center = [7.2 10.9];
@@ -12,4 +13,9 @@ switch platelayout
             plate.width_mm = 43.2;
             plate.well_one_center = [5.4, 5.4];
             plate.interwell_dist  = [16.2, 11.79];
+        case 'nunc-96' % nunc spec.
+            error('nunc-96 is not yet defined.');
+%         plate.well_one_center = [14.32 11.25];
+    otherwise 
+        error('This plate definition is not found. Add specs to platedef.m');
     end
