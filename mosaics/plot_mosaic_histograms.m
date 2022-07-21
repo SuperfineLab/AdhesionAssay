d64572e447bd10066c792641eba3a5caa6fb4965
Jeremy Cribb <cribb@cs.unc.edu>
2022-07-21 14:55:29 -0400

    Tweaking computation to optimize plot.

--- mosaics/plot_mosaic_histograms.m
+++ mosaics/plot_mosaic_histograms.m
@@ -1,30 +1,35 @@
 
 
-file_list = dir('well*200ms.mat');
+file_list = dir('well*50ms.mat');
 f1 = figure;
 f2 = figure;
 for m = 1:15
     well = load( file_list(m).name);
+    
     N = height(well.mosaic);
     for k = 1:N
         tmp = double(well.mosaic.Image{k});
-        frame(:,:,k) = tmp;
+        well_frames(:,:,k) = tmp;
         frame_mean(k,m) = mean(tmp(:));
     end
-    figure(f1)
-    subplot(3,5,m)
-    histogram(frame_mean(:,m))
-    ylim([0 110])
-    xlim([4000 8000])
 
-    figure(f2)
-    subplot(3,5,m)
-    histogram(frame(:));
-    ax = gca;
-    ax.YScale = 'log';
+    mosaic_pixels(:,m) = well_frames(:);
 
-end 
-clear frame
+end
+
+% figure(f1)
+% subplot(3,5,m)
+% histogram(frame_mean(:,m))
+% ylim([0 110])
+% xlim([4000 8000])
+% 
+% figure(f2)
+% subplot(3,5,m)
+% histogram(well_frames(:));
+% ax = gca;
+% ax.YScale = 'log';
+
+% clear frame
 % figure;
 % for m = 1:15
 % 
