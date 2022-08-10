8740f02acb8147bd17f9ae427dcf93254155f0b0
Jeremy Cribb <cribb@cs.unc.edu>
2022-08-10 11:06:24 -0400

    Tweaks to plotting. This script needs factoring into a function.

--- mosaics/plot_mosaics_from_plate.m
+++ mosaics/plot_mosaics_from_plate.m
@@ -1,9 +1,11 @@
 clear q
 subplot = @(m,n,p) subtightplot(m, n, p, [0.01 0.005], [0.1 0.01], [0.1 0.01]);
 % function h =     subtightplot(m, n, p, gap,         marg_h,     marg_w,   varargin)
-f = figure;
-exptime(1:15) = 200;
-for k = 1:length(well)
+
+well = 1:15;
+exptime(1:15) = 8;
+calibum = 0.692;
+for k = 1:5
 % for k = [5:-1:1,10:-1:6,15:-1:11]
     
     fname = ['well-' num2str(well(k), '%02i') '_10x_' num2str(exptime(k)) 'ms'];
@@ -11,7 +13,8 @@ for k = 1:length(well)
     m = load([fname '.mat']);
     m = m.mosaic;
     im = imtile(m.Image, 'GridSize', [13 13]); 
-%     im = im(3500:7500, 4000:10000);  
+    imwrite(im, [fname '.tif'], 'tif');
+    im = im(3500:7500, 4000:10000);  
     q(:,:,k) = im;
     sumIntensity(k,1) = sum(im(:));
     myscale = 0.25;
@@ -22,13 +25,25 @@ for k = 1:length(well)
 end
 
 f = figure;
-for k = [5:-1:1, 10:-1:6, 15:-1:11]
+for k = [5:-1:1]%, 10:-1:6, 15:-1:11]
     figure(f);
     subplot(3,5,k);
     imagesc(X_mm, Y_mm, q(:,:,k)); 
     colormap(gray); 
     ax = gca; 
-    ax.CLim = [3400 5200];    
+    ax.CLim = [3500 5000];    
     xlabel('[mm]');
     ylabel('[mm]');    
 end
+
+
+% for k = [5:-1:1, 10:-1:6, 15:-1:11]
+%     figure(f);
+%     subplot(3,5,k);
+%     colormap(gray); 
+%     ax = gca; 
+%     ax.CLim = [4000 6000];    
+%     xlabel('[mm]');
+%     ylabel('[mm]');    
+% end
+
