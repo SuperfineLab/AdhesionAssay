041a64fcfe3fe71d162f58890a09f9701c4cfca9
Jeremy A. Cribb <jcribb@unc.edu>
2023-08-21 16:19:21 -0400

    Cosmetic changes.

--- ba_config.m
+++ ba_config.m
@@ -1,44 +1,47 @@
 % config ixion for vid-capture on the command line
-ExposureTime = 8; 
+% ba_config
 
-hw.zhand = NaN;
+[~, hostname] = system('hostname');
+
+switch upper(strip(hostname))
+    case "ZINC"
+        scopename = 'Artemis';
+        scope_comport = 'COM2';
+        ludl_comport = 'COM6';
+    case "CERIUM"
+        scopename = 'Artemis';
+        scope_comport = 'COM2';
+        ludl_comport = 'COM4';
+    case "CROMIUM"
+        scopename = 'Ixion';        
+    otherwise
+        error('This system is not configured. Edit ba_config.');
+end
+
+
+
+switch(scopename)
+    case 'Ixion'
+        hw.zhand = NaN;
+
+    case 'Artemis'
+        hw.zhand = tm_initz('Artemis');
+
+        hw.ludl = stage_open_Ludl(ludl_comport);
+        hw.ludl.speed = 20000;
+        hw.ludl.accel = 0.01;
+        
+        hw.scope = scope_open(scope_comport);
+
+
+end
+
 
 viewOps.cmin = 0;
 viewOps.cmax = 65535;
-viewOps.gain = 12;
+viewOps.gain = 15;
 viewOps.focusTF = false;
-viewOps.exptime = ExposureTime;
-viewOps.autoscaleTF = true;
+viewOps.exptime = 8; 
 
-Video = flir_config_video('Grasshopper3', 'F7_Raw16_1024x768_Mode2', ExposureTime);
+Video = flir_config_video('Grasshopper3', 'F7_Raw16_1024x768_Mode2', viewOps.exptime);
 
-clear ExposureTime
-
-% % % ba_config
-% % 
-% % [~, hostname] = system('hostname');
-% % 
-% % switch(hostname)
-% %     case 'zinc'
-% %         scopename = 'Artemis';
-% %     case 'cromium'
-% %         scopename = 'Ixion';
-% %     otherwise
-% %         error('This system is not configured. Edit ba_config.');
-% % end
-% % 
-% % 
-% % 
-% % switch(scopename)
-% %     case 'Ixion'
-% %         hw.zhand = NaN;
-% %         viewOps.cmin = 0;
-% %         viewOps.cmax = 65535;
-% %         viewOps.gain = 12;
-% %         viewOps.focusTF = false;
-% %         viewOps.exptime = 8;
-% %     case 'Artemis'
-% %         
-% % end
-% % 
-% % 
