cb571fc8e1213af022eb3bf77d505b3edf548e5c
Jeremy A. Cribb <jcribb@unc.edu>
2023-11-07 11:49:38 -0500

    Added a function for calculating the distance matrix between objects in a field of view and updated nndist to use the dist matrix to avoid repeating code.

--- control/nndist.m
+++ control/nndist.m
@@ -8,19 +8,8 @@ if isempty(c)
     return
 end
 
-x = c(:,1);
-y = c(:,2);
+dist = distmatrix(c);
 
-% Tile out the positions into a square matrix
-X = repmat(x, 1, numel(x));
-Y = repmat(y, 1, numel(y));
-
-% X and Y distances from other beads
-Xdist = X - transpose(X);
-Ydist = Y - transpose(Y);
-dist = sqrt(Xdist.^2 + Ydist.^2);
-
-% Eliminate the zeros along the diagonal
 dist( dist == 0 ) = NaN;
 
 % The nearest neighbor for each bead is the minimum distance along each row
