f32c162e07734882fd4488fc0675dfb5ebd9fb4a
Jeremy A. Cribb <jcribb@unc.edu>
2023-12-01 11:30:30 -0500

    Changed to match new function name.

--- analysis/ba_process_expt.m
+++ analysis/ba_process_expt.m
@@ -116,7 +116,7 @@ switch modeltype
     case 'erf'        
         [DetachForce, fits] = ba_plate_detachmentforces_erf(Data, aggregating_variables, true);
     case 'exponential'
-        [DetachForce, fits] = ba_plate_detachmentforces(Data, aggregating_variables, true);
+        [DetachForce, fits] = ba_plate_detachmentforces_exp(Data, aggregating_variables, true);
     otherwise
         error('Missing or unknown model type.');
 end
