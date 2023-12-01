70609af3aaf0f1bff19d14dab4c98a270681988a
Jeremy A. Cribb <jcribb@unc.edu>
2023-12-01 11:28:34 -0500

    Removed plotting code and put into own function.

--- analysis/ba_fit_erf.m
+++ analysis/ba_fit_erf.m
@@ -8,12 +8,10 @@ function [fitresult, gof] = ba_fit_erf(logforce, pct_left, weights, Nterms, plot
         Nterms = 2;
     end
 
-    % log transform the force
+%     % log transform the force
 %     logforce = log10(force);
     [logforce, pct_left, weights] = prepareCurveData( logforce, pct_left, weights );
     
-%     [tmpa,tmpb] = get_good_startpoint(logforce, pct_left, weights);
-                    
     % Set up fittype and options.
     opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 
@@ -24,14 +22,12 @@ function [fitresult, gof] = ba_fit_erf(logforce, pct_left, weights, Nterms, plot
             %             a  am   as  
             opts.Lower = [0 -Inf  0  ];
             opts.Upper = [1  Inf  Inf];
-
         case 2
             ft = fittype( '1/2*(a*erfc(((x)-am)/(sqrt(2)*as))+(1-a)*erfc(((x)-bm)/(sqrt(2)*bs)))', 'independent', 'x', 'dependent', 'y' );
             opts.StartPoint = [0.825816977489547 0.0781755287531837 0.442678269775446 0.106652770180584 0.961898080855054];
             %             a  am   as   bm   bs
             opts.Lower = [0 -Inf  0   -Inf  0  ];
-            opts.Upper = [1  Inf  Inf  Inf  Inf];
-        
+            opts.Upper = [1  Inf  Inf  Inf  Inf];       
         otherwise
             error('Unknown number of terms. Select "1" or "2" terms.');
     end
@@ -41,25 +37,7 @@ function [fitresult, gof] = ba_fit_erf(logforce, pct_left, weights, Nterms, plot
     opts.MaxIter = 24000;
     opts.Weights = weights;
     
-
     % Fit model to data.
     [fitresult, gof] = fit( logforce, pct_left, ft, opts );
-
-    if plotTF
-        % Plot fit with data.
-        figure( 'Name', 'erf fit' );
-        h = plot( fitresult, logforce, pct_left, 'predobs' );
-        legend( h, 'y vs. x with w', 'hbe', 'Lower bounds (hbe)', 'Upper bounds (hbe)', 'Location', 'NorthEast', 'Interpreter', 'none' );
-        % Label axes
-        xlabel( 'log(force [nN])', 'Interpreter', 'none' );
-        ylabel( 'Fraction Left', 'Interpreter', 'none' );
-        grid on
-
-        errlength = log10(10.^logforce + 1./weights);
-        e = errorbar( gca, logforce, pct_left, errlength, 'horizontal');
-        e.LineStyle = 'none';
-        e.Color = 'b';
-
-    end
 
 end
