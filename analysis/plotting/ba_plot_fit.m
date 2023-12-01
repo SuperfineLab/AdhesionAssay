5bf80484514a2876295b9734ddfe53ad1a0c92bd
Jeremy A. Cribb <jcribb@unc.edu>
2023-12-01 14:28:58 -0500

    Now includes error handling in case the input fit is bad AND can take a figure handle on the input (for plotting into an already existing figure).

--- analysis/plotting/ba_plot_fit.m
+++ analysis/plotting/ba_plot_fit.m
@@ -1,11 +1,15 @@
-function ba_plot_fit(fitresult, force, errforce, pct_left)
+function ba_plot_fit(fitresult, force, errforce, pct_left, figurehandle)
+
+if nargin < 5 || isempty(figurehandle)
+    figurehandle = figure;
+end
 
     logforce = log10(force);
     errlogforce = log10(force + errforce)-log10(force);
-
             
-        % Plot fit with data.
-        figure( 'Name', 'erf fit' );
+    % Plot fit with data.
+    figurehandle.Name = 'erf fit';    
+    try
         h = plot( fitresult, logforce, pct_left, 'predobs' );
         legend( h, 'y vs. x with w', 'hbe', 'Lower bounds (hbe)', 'Upper bounds (hbe)', 'Location', 'NorthEast', 'Interpreter', 'none' );
         % Label axes
@@ -18,5 +22,9 @@ function ba_plot_fit(fitresult, force, errforce, pct_left)
             e.LineStyle = 'none';
             e.Color = 'b';
         hold off
-        drawnow
+    catch
+        clf;
+        text(0.5,0.5,'Error while plotting.');
+    end
+    drawnow
 end
