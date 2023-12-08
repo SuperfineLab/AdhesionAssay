cf8bf3cf2d043c623c85ee522130446462d97f8b
Jeremy A. Cribb <jcribb@unc.edu>
2023-12-08 12:48:37 -0500

    Pulled out old commented-out code and unused functions.

--- analysis/ba_make_ForceTable.m
+++ analysis/ba_make_ForceTable.m
@@ -42,9 +42,6 @@ for k = 1:length(evtfilelist)
    origfname = [basename '.csv'];
    metafname = [basename '.meta.mat'];
    
-
-   
-   
    % Load data from metadata file. Ultimately, use this as indexing file 
    % when combining data for an entire experiment
    metadata = load(metafname);
@@ -89,114 +86,14 @@ if isstring(ForceTable.SpotID)
     ForceTable.SpotID = double(ForceTable.SpotID);
 end
 
-% ForceTable.Filename = [];
-% ForceTable = vertcat(ForceTable{:});
-% FileTable = vertcat(FileTable{:});
-% FileTable.PlateID = categorical(repmat(string(PlateID),height(FileTable),1));
-% FileTable = movevars(FileTable, 'PlateID', 'before', 'Fid');
-% BeadInfoTable = vertcat(BeadInfoTable{:});
-
-% % % [g, grpT] = findgroups(FileTable(:,aggregating_variables));
-% % % NStartingBeads(:,1) = splitapply(@sum, FileTable.FirstFrameBeadCount, g);
-% % % NStartingBeadsT = [grpT, table(NStartingBeads)];
-% % % 
-% % % T = join(ForceTable, FileTable(:,{'Fid', aggregating_variables{:}}));
-% % % T = join(T, NStartingBeadsT);
-% % % % T = sortrows(T, {'Filename', 'Force'}, {'ascend', 'ascend'});
-% % % [g, grpT] = findgroups(T(:,aggregating_variables));
-% % % 
-% % % foo = splitapply(@(x1,x2,x3,x4){sa_fracleft(x1,x2,x3,x4)}, T.Fid, T.SpotID, T.Force, T.NStartingBeads, g);
-% % % fooM = cell2mat(foo);
-% % % 
-% % % Tmp.Fid = fooM(:,1);
-% % % Tmp.SpotID = fooM(:,2);
-% % % Tmp.Force = fooM(:,3);
-% % % Tmp.FractionLeft = fooM(:,4);
-% % % 
-% % % Tmp = struct2table(Tmp);
-% % % ForceTable = join(ForceTable, Tmp);
 
 cd(rootdir);
  
-% outs.FileTable = FileTable;
-% outs.ForceTable = ForceTable;
-% outs.BeadInfoTable = BeadInfoTable;
-% % 
-% % % switch modeltype
-% % %     case 'linear'
-% % %         [DetachForce, fits] = ba_plate_detachmentforces_linear(outs, aggregating_variables, true);
-% % %     case 'erf'        
-% % %         [DetachForce, fits] = ba_plate_detachmentforces_erf(outs, aggregating_variables, true);
-% % %     case 'exponential'
-% % %         [DetachForce, fits] = ba_plate_detachmentforces(outs, aggregating_variables, true);
-% % %     otherwise
-% % %         error('Missing or unknown model type.');
-% % % end
 outs.ForceTable = ForceTable;
-% outs.DetachForceTable = DetachForce;
 
 end
 
 
-function sm = shorten_metadata(metadata)
-    sm.Fid = metadata.File.Fid;
-    sm.FullFilename = string(fullfile(metadata.File.Binpath, metadata.File.Binfile));
-    sm.StartTime = metadata.Results.TimeHeightVidStatsTable.Time(1);
-    sm.MeanFps  = 1 ./ mean(diff(metadata.Results.TimeHeightVidStatsTable.Time*86400));
-    sm.ExposureTime = metadata.Video.ExposureTime;
-    sm.Binfile = string(metadata.File.Binfile);
-    sm.SampleName = string(metadata.File.SampleName);
-    sm.BeadChemistry = string(metadata.Bead.SurfaceChemistry);
-    sm.SubstrateChemistry = string(metadata.Substrate.SurfaceChemistry);
-    sm.MagnetGeometry = string(metadata.Magnet.Geometry);
-    sm.Media = string(metadata.Medium.Name);
-    sm.Buffer = string(metadata.Medium.Buffer);
-    sm.pH = metadata.Medium.pH;
-    
-    [a,b] = regexpi(sm.Binfile,'_DF(\d*)_', 'match', 'tokens');
-
-%     if ~isempty(b) || lower(sm.Media) == "int" || lower(sm.Media) == "intnana"
-%         DF = str2double(b{1});
-     idx = contains(metadata.Medium.Components.Name, "Sialic Acid");
-    if sum(idx)>0
-        sm.MediumNANAConc = categorical(metadata.Medium.Components.Conc(idx));
-%     elseif lower(sm.Media) == "int" || lower(sm.Media) == "intnana"
-%         sm.MediumNANAConc
-    else
-        sm.MediumNANAConc = categorical(0);
-    end
-    
-    
-    sm.MediumViscosity = metadata.Medium.Viscosity;
-    sm.Calibum = metadata.Scope.Calibum;    
-    
-    if isfield(metadata.Substrate, 'LotNumber')
-        sm.SubstrateLotNumber = string(metadata.Substrate.LotNumber);
-    end
-    
-    sm = struct2table(sm);
-    
-    sm.BeadChemistry = categorical(sm.BeadChemistry);
-    sm.SubstrateChemistry = categorical(sm.SubstrateChemistry);
-    sm.MagnetGeometry = categorical(sm.MagnetGeometry);  
-    sm.pH = categorical(sm.pH);
-    sm.Media = categorical(sm.Media);
-end
-
-function outs = sa_fracleft(fid, spotid, force, startCount)
-
-    force = force(:);
-    Nforce = length(force);   
-    
-    % I do not really understand how this determines "rank" of force, but
-    % it does and outputs the fraction left attached
-    [~,Fidx] = sort(force, 'ascend');
-    [~,Frank] = sort(Fidx, 'ascend');    
-    
-    fracleft = 1-(Frank ./ startCount);
-
-    outs = [fid(:), spotid(:), force(:), fracleft(:)];
-end
 
 
 
