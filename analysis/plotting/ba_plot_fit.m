986a63b666755a57f68690e09da65747e6bc78ff
Jeremy A. Cribb <jcribb@unc.edu>
2024-02-21 12:30:37 -0500

    Added figure handle as output to erf fitting plots.

--- analysis/plotting/ba_plot_fit.m
+++ analysis/plotting/ba_plot_fit.m
@@ -1,22 +1,22 @@
-function ba_plot_fit(fitresult, force, errforce, pct_left, figurehandle)
+function figh = ba_plot_fit(fitresult, force, errforce, pct_left, figh)
 
-if nargin < 5 || isempty(figurehandle)
-    figurehandle = figure;
+if nargin < 5 || isempty(figh)
+    figh = figure;
 end
 
     logforce = log10(force);
     errlogforce = log10(force + errforce)-log10(force);
             
     % Plot fit with data.
-    figurehandle.Name = 'erf fit';    
+    figh.Name = 'erf fit';    
     clf
     hold on
     try
         
 %             h = plot( fitresult, logforce, pct_left, 'predobs' );
 %             legend( h, 'y vs. x with w', 'hbe', 'Lower bounds (hbe)', 'Upper bounds (hbe)', 'Location', 'NorthEast', 'Interpreter', 'none' );
-            h = plot( fitresult, logforce, pct_left );
-            legend( h, 'y vs. x with w', 'hbe', 'Location', 'NorthEast', 'Interpreter', 'none' );
+            axh = plot( fitresult, logforce, pct_left );
+            legend( axh, 'y vs. x with w', '', 'Location', 'NorthEast', 'Interpreter', 'none' );
 
             xlabel( 'log(force [nN])', 'Interpreter', 'none' );
             ylabel( 'Fraction Left', 'Interpreter', 'none' );
