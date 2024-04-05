cf05f6039cd57ad47e286c37bc1db377e55792a9
Jeremy A. Cribb <jcribb@unc.edu>
2024-04-05 12:06:36 -0400

    Removed FractionLeft computation from this function and placed it into its own function.

--- analysis/ba_calc_BeadsLeft.m
+++ analysis/ba_calc_BeadsLeft.m
@@ -1,4 +1,6 @@
-function [newFileTable,newForceTable] = ba_calc_BeadsLeft(Data, aggregating_variables)
+function newFileTable = ba_calc_BeadsLeft(Data)
+% XXX @stephensnare Add documentation for this function.
+
 % Number of stuck beads is equal to the starting number of beads minus
 % the number of Force approximations we made during our tracking
 % clean-up for the velocity calculation.
@@ -18,28 +20,6 @@ function [newFileTable,newForceTable] = ba_calc_BeadsLeft(Data, aggregating_vari
 
     newFileTable.LastFrameBeadCount = LastFrameBeadCount;
 
-    [g, grpT] = findgroups(newFileTable(:,aggregating_variables));
-    NStartingBeads(:,1) = splitapply(@sum, newFileTable.FirstFrameBeadCount, g);
-    NStartingBeadsT = [grpT, table(NStartingBeads)];
-    
-    T = join(ForceTable, newFileTable(:,{'Fid', aggregating_variables{:}})); %#ok<CCAT> 
-    T = join(T, NStartingBeadsT);
-    % T = sortrows(T, {'Filename', 'Force'}, {'ascend', 'ascend'});
-    [g, grpT] = findgroups(T(:,aggregating_variables));
-    
-    foo = splitapply(@(x1,x2,x3,x4){sa_fracleft(x1,x2,x3,x4)}, T.Fid, T.SpotID, T.Force, T.NStartingBeads, g);
-    fooM = cell2mat(foo);
-    
-    Tmp.Fid = fooM(:,1);
-    Tmp.SpotID = fooM(:,2);
-    Tmp.Force = fooM(:,3);
-    Tmp.FractionLeft = fooM(:,4);
-    
-    Tmp = struct2table(Tmp);
-    newForceTable = join(ForceTable, Tmp);
-
-    newForceTable.Properties.VariableUnits{'FractionLeft'} = '[]';
-    newForceTable.Properties.VariableDescriptions{'FractionLeft'} = 'Fraction of beads remaining on substrate';
 end
 
 
