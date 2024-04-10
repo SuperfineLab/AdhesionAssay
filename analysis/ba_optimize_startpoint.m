7fa9457a797fcd023825e1b6bbca5d9dd2a2a3a4
Jeremy A. Cribb <jcribb@unc.edu>
2024-04-10 17:43:52 -0400

    Tweaked support functions for different fitting algorithms. This code may not work at this time.

--- analysis/ba_optimize_startpoint.m
+++ analysis/ba_optimize_startpoint.m
@@ -1,4 +1,4 @@
-function optstart = ba_optimize_startpoint(logforce_nN, fractionLeft, weights, Nmodes)
+function optstart = ba_optimize_startpoint(logforce_nN, logforceinterval, fractionLeft, weights, Nmodes)
 % XXX @jeremy TODO: Add documentation for this function.
 %
 % Assembles a "protofit" that optimizes the fitting startpoints for the
@@ -10,29 +10,35 @@ function optstart = ba_optimize_startpoint(logforce_nN, fractionLeft, weights, N
 
 fout = ba_setup_fit('erf-old', weights, Nmodes);
 
-% optstart = optimize_startpoint(fitfcn, fout.StartPoint, logforce_nN, fractionLeft, fout.lb, fout.ub);
-optstart = optimize_startpoint_w(fout.fcn, fout.StartPoint, logforce_nN, fractionLeft, weights, fout.lb, fout.ub);
-% optstart = optimize_startpoint_ga(fout.fcn, fout.StartPoint, logforce_nN, fractionLeft, weights, fout.lb, fout.ub);
+% optstart = optimize_lsqcurvefit(fout.fcn, fout.StartPoint, logforce_nN, fractionLeft, fout.lb, fout.ub);
+optstart = optimize_lsqnonlin(fout.fcn, fout.StartPoint, logforce_nN, fractionLeft, weights, fout.lb, fout.ub);
+% optstart = optimize_lsqnonlin(fout.fcn, fout.StartPoint, logforce_nN, fractionLeft, [], fout.lb, fout.ub);
+% optstart = optimize_ga(fout.fcn, fout.StartPoint, logforce_nN, fractionLeft, weights, fout.lb, fout.ub);
 
-% f = figure;
-% plot( logforce_nN, fractionLeft, 'r.', ...
-%       logforce_nN, fout.fcn(optstart, logforce_nN), 'b-');
-% xlabel('log_{10}(Force [nN])');
-% ylabel('Factor left');
-% legend('data', 'fit');
-% title(['Nterms = ', num2str(Nmodes)]);
-% drawnow
+gray = [0.5 0.5 0.5];
+
+f = figure;
+hold on
+plot( logforce_nN, fractionLeft, 'Color', gray, 'Marker', '.', 'LineStyle', 'none');
+plot( logforceinterval(:,1), fractionLeft, 'Color', gray+0.3, 'LineStyle', '-');
+plot( logforceinterval(:,2), fractionLeft, 'Color', gray+0.3, 'LineStyle', '-');      
+plot( logforce_nN, fout.fcn(optstart, logforce_nN), 'Color', 'r', 'LineStyle', '-');
+hold off
+xlabel('log_{10}(Force [nN])');
+ylabel('Factor left');
+legend('data', 'fit');
+title(['Nterms = ', num2str(Nmodes)]);
+drawnow
 
 end
 
 
 % Solve best fit using global optimization and genetic algorithm
-function optstart = optimize_startpoint_ga(fitfcn, p0, logforce_nN, fractionLeft, weights, lb, ub)
+function optstart = optimize_ga(fitfcn, p0, logforce_nN, fractionLeft, weights, lb, ub)
 
     [costfunction, costerror] = calculated_error(p0, fitfcn, logforce_nN, fractionLeft, weights);
 
     opts = optimoptions(@ga, 'Display', 'final');
-%     probopts = optimoptions(@lsqnonlin, 'Display', 'final');
     problem = createOptimProblem('ga','x0',p0,'objective',costfunction,...
         'lb',lb,'ub',ub,options=opts);
 
@@ -44,10 +50,9 @@ function optstart = optimize_startpoint_ga(fitfcn, p0, logforce_nN, fractionLeft
 end
 
 
-function optstart = optimize_startpoint(fitfcn, p0, logforce_nN, fractionLeft, lb, ub)
+function optstart = optimize_lsqcurvefit(fitfcn, p0, logforce_nN, fractionLeft, lb, ub)
 
-    opts = optimset('Display', 'off');
-    [xfitted,errorfitted] = lsqcurvefit(fitfcn,p0,logforce_nN,fractionLeft,lb,ub, opts);
+    opts = optimset('Display', 'off');    
     
     probopts = optimoptions(@lsqcurvefit, 'Display', 'off');
 
@@ -56,32 +61,29 @@ function optstart = optimize_startpoint(fitfcn, p0, logforce_nN, fractionLeft, l
     
 %     ms = MultiStart('PlotFcns',@gsplotbestf);
     ms = MultiStart("Display","off");
+
+    % [X,FVAL,EXITFLAG,OUTPUT,SOLUTIONS] = run(ms, problem, 100);
     [optstart,errormulti] = run(ms,problem,100);
 end
 
 
-function optstart = optimize_startpoint_w(fitfcn, p0, logforce_nN, fractionLeft, weights, lb, ub)
+
+function optstart = optimize_lsqnonlin(fitfcn, p0, logforce_nN, fractionLeft, weights, lb, ub)
+
+    if isempty(weights)
+        weights = ones(size(logforce_nN));
+    end
 
     costfunction = @(p) weights .* (fitfcn(p,logforce_nN)-fractionLeft).^2;
 
-    opts = optimset('Display',      'off', ...
-                    'MaxFunEvals',   26000, ...
-                    'MaxIter',       24000, ...
-                    'TolFun',        1e-07, ...
-                    'TolX',          1e-07, ...
-                    'DiffMinChange', 1e-08, ...
-                    'DiffMaxChange', 0.01);
-
-    [myfit,myfiterror] = lsqnonlin(costfunction,p0,lb,ub,opts);
-    
     probopts = optimoptions(@lsqnonlin, 'Display', 'off'); % Display: 'final' or 'off'
     
     problem = createOptimProblem('lsqnonlin','x0',p0,'objective',costfunction,...
         'lb',lb,'ub',ub,options=probopts);
     
-%     ms = MultiStart('PlotFcns',@gsplotbestf);
+    ms = MultiStart('PlotFcns',@gsplotbestf);
     ms = MultiStart("Display","off");
-    [optstart,errormulti] = run(ms,problem,100);
+    [optstart,errormulti] = run(ms,problem,200);
 end
 
 
