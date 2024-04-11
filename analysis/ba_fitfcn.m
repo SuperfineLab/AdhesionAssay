3034229880ad6ce4285aa6baaa646b0252b24352
Jeremy A. Cribb <jcribb@unc.edu>
2024-04-11 14:26:28 -0400

    Started stripping out the old model equation type. Still may not work exactly right.

--- analysis/ba_fitfcn.m
+++ analysis/ba_fitfcn.m
@@ -1,39 +1,47 @@
-function varargout = ba_fitfcn(fittype, Nmodes)
+function fitfcn = ba_fitfcn(Nmodes)
 
-    % old form
-    fitfcn{1} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3)))));
-    fitfcn{2} = @(p, Fd)(1/2*(  p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
-                             (1-p(1))*erfc(((Fd)-p(4))/(sqrt(2)*p(5)))));
-
-    % new form
-    fitfcnNew{1} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3)))));
-    fitfcnNew{2} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
-                                 p(4)*erfc(((Fd)-p(5))/(sqrt(2)*p(6))))); 
-    fitfcnNew{3} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
-                                 p(4)*erfc(((Fd)-p(5))/(sqrt(2)*p(6))) + ...
-                                 p(7)*erfc(((Fd)-p(8))/(sqrt(2)*p(9))))); 
-
-    switch fittype
-        case "erf-old"
-            if Nmodes > 2
-                warning('This model not defined for more than 2 modes. Resetting to two modes.');
-                Nmodes = 2;
-            end
-            fiteq = fitfcn{Nmodes};
-            Nparams = 2*Nmodes + 1;
-        case "erf-new"
-            fiteq = fitfcnNew{Nmodes};
-            Nparams = 3*Nmodes;
-        otherwise
-            error('Fit type not defined.');           
+    Nparams = Nmodes*3;
+    plist = reshape(1:Nparams,3,[])';
+    for k = 1:Nmodes
+       eqstr{1,k} = compose('p(%d)*erfc((Fd-p(%d))/(sqrt(2)*p(%d)))',plist(k,:));
     end
+    eqstr = join(string(eqstr), ' + ');
+    fitfcn = str2func(['@(p,Fd)(1/2*(', char(eqstr), '))']);
 
-    switch nargout
-        case 1
-            varargout{1} = fiteq;
-        case 2
-            varargout{1} = fiteq;
-            varargout{2} = Nparams;
-    end
+%     % old form
+%     fitfcn{1} = @(p, Fd)(1/2*(  p(1) *erfc(((Fd)-p(2))/(sqrt(2)*p(3)))));
+%     fitfcn{2} = @(p, Fd)(1/2*(  p(1) *erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
+%                              (1-p(1))*erfc(((Fd)-p(4))/(sqrt(2)*p(5)))));
+% 
+%     % new form
+%     fitfcnNew{1} = @(p, Fd)(1/2*(p(1)*erfc((Fd)-p(2))/(sqrt(2)*p(3)))));
+%     fitfcnNew{2} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
+%                                  p(4)*erfc(((Fd)-p(5))/(sqrt(2)*p(6))))); 
+%     fitfcnNew{3} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
+%                                  p(4)*erfc(((Fd)-p(5))/(sqrt(2)*p(6))) + ...
+%                                  p(7)*erfc(((Fd)-p(8))/(sqrt(2)*p(9))))); 
+% 
+%     switch fittype
+%         case "erf-old"
+%             if Nmodes > 2
+%                 warning('This model not defined for more than 2 modes. Resetting to two modes.');
+%                 Nmodes = 2;
+%             end
+%             fiteq = fitfcn{Nmodes};
+%             Nparams = 2*Nmodes + 1;
+%         case "erf-new"
+%             fiteq = fitfcnNew{Nmodes};
+%             Nparams = 3*Nmodes;
+%         otherwise
+%             error('Fit type not defined.');           
+%     end
+% 
+%     switch nargout
+%         case 1
+%             varargout{1} = fitfcn;
+%         case 2
+%             varargout{1} = fitfcn;
+%             varargout{2} = Nparams;
+%     end
 
 end
--- analysis/ba_setup_fit.m
+++ analysis/ba_setup_fit.m
@@ -1,73 +1,48 @@
-function fout = ba_setup_fit(fitname, weights, Nmodes, startpoint)
+function fout = ba_setup_fit(Nmodes, weights, startpoint)
 % XXX @jeremy TODO: Write documentation
 %
-    if nargin < 1 || isempty(fitname)
-        fitname = 'erf-new';
-    end
+
 
 %
 % XXX @jeremy TODO: strip out all of this "old-erf" modeling stuff. This
 % requires setting up the goodness of fit metrics for an lsqcurvefit (or
 % nlinlsqcurve fit with constraints on input parameters a, b, c, etc
 %
+
     % new form
-    [fitfcn, Nparams] = ba_fitfcn(fitname, Nmodes);
+    fitfcn = ba_fitfcn(Nmodes);
+    Nparams = Nmodes * 3;
 
-    switch lower(fitname)
-        case 'erf-old'
-            switch Nmodes
-                case 1
-                    lb = [0, -Inf, 0];
-                    ub = [1,  Inf, Inf];
-                case 2
-                    lb = [0, -Inf,   0, -Inf, 0];
-                    ub = [1,  Inf, Inf,  Inf, Inf];
-            end
 
-        case 'erf-new'
-            % Upper and Lower bounds for *ONE MODE* parameter fitting
-            %     [a,   am,  as]
-            lb1 = [0, -Inf,   0];
-            ub1 = [1,  Inf, Inf];
-            
-            % Tile out bounds according to how many modes in the fit
-            lb = repmat(lb1, 1, Nmodes);
-            ub = repmat(ub1, 1, Nmodes);
-    end
+    % Upper and Lower bounds for *ONE MODE* parameter fitting
+    %     [a,   am,  as]
+    lb1 = [0, -Inf,   0];
+    ub1 = [1,  Inf, Inf];
+    
+    % Tile out bounds according to how many modes in the fit
+    lb = repmat(lb1, 1, Nmodes);
+    ub = repmat(ub1, 1, Nmodes);
 
 
     %
     % Handling startpoints
     %
+    % Default parameters are set according to the number of modes and 
+    % equally spaced from low-high limits, which are assumed to be 
+    % between 10^-1.5 and 10^2 nanoNewtowns.
 
-    % **OLD** default starting points and bounds-setting if none are available    
-    if strcmpi(fitname, 'erf-old') && (nargin < 4 || isempty(startpoint))
-        logentry('Using predefined startpoint for old erfmode fits.')
-        switch Nmodes
-            case 1
-                pstart = [0.82582 0.07818 0.44268];
-            case 2
-                pstart = [0.82582 0.07818 0.44268 0.10666 0.96190];
-            otherwise
-                error('Old erf fit equation not defined for Nmodes>2.')
-        end
+    logforcelimitLow = -1.5;
+    logforcelimitHigh = 2;
+    logforcerange = abs(logforcelimitHigh - logforcelimitLow);
+    logforcestep = logforcerange / (Nmodes+1);
 
-    elseif nargin < 4 || isempty(startpoint) || numel(startpoint) ~= Nparams
-        % Handle (i.e., guess) the starting locations for parameters according 
-        % to the number of modes and equally spaced from low-high limits,
-        % which are assumed to be between 10^-1.5 and 10^2 nanoNewtowns.
-        logforcelimitLow = -1.5;
-        logforcelimitHigh = 2;
-        logforcerange = abs(logforcelimitHigh - logforcelimitLow);
-        logforcestep = logforcerange / (Nmodes+1);
+    if nargin < 3 || isempty(startpoint) || numel(startpoint) ~= Nparams
     
-        p0 =  [1/Nmodes, logforcelimitLow+0*logforcestep, 0.5; ...
-               1/Nmodes, logforcelimitLow+1*logforcestep, 0.5; ...
-               1/Nmodes, logforcelimitLow+2*logforcestep, 0.5];
+        p0 = repmat([1/Nmodes, NaN, 0.5], Nmodes, 1);
+        p0(:,2) = logforcelimitLow+transpose(0:Nmodes-1)*logforcestep;
     
         % Spin out starting locations according to number of modes
-        pstart(1,:) = reshape(transpose(p0),1,[]);
-        pstart = pstart(1:Nparams);
+        pstart(1,:) = reshape(transpose(p0),1,[]);        
         
     else
         pstart = startpoint;
@@ -78,106 +53,61 @@ function fout = ba_setup_fit(fitname, weights, Nmodes, startpoint)
     beq = 1;
 
     % Assemble default options structure 
-    opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-    opts.Display = 'Off';
-    opts.MaxFunEvals = 26000;
-    opts.MaxIter = 24000;
-    opts.Weights = weights;
-    opts.TolFun = 1e-07;
-    opts.TolX = 1e-07;
-    opts.DiffMinChange = 1e-08;
-    opts.DiffMaxChange = 0.01;
-    opts.StartPoint = pstart;
-    opts.Lower = lb;
-    opts.Upper = ub;
+    opts = setup_fitoptions('lsqnonlin', [], pstart, lb, ub);
 
-    fout.fitname = fitname;
+    
     fout.fcn = fitfcn;    
     fout.Nmodes = Nmodes;
     fout.Nparams = Nparams;
-    fout.opts = opts;
-%     fout.logforcelimitHigh = logforcelimitHigh;
-%     fout.logforcelimitLow = logforcelimitLow;
+%     fout.opts = opts;
+    fout.logforcelimitHigh = logforcelimitHigh;
+    fout.logforcelimitLow = logforcelimitLow;
     fout.StartPoint = pstart;
     fout.lb = lb;
     fout.ub = ub;
-
     fout.Aeq = Aeq;
     fout.beq = beq;
 end
 
 
-function varargout = ba_fitfcn(fitname, Nmodes)
+% OPTIMOPTIONS
+function opts = setup_fitoptions(method, weights, startpoint, lb, ub)
+    
+    opts = optimoptions(method);
 
-    % old form
-    fitfcn{1} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3)))));
-    fitfcn{2} = @(p, Fd)(1/2*(  p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
-                             (1-p(1))*erfc(((Fd)-p(4))/(sqrt(2)*p(5)))));
+    switch method
+        case 'fmincon'
+            opts.Algorithm = 'interior-point';
+            opts.UseParallel = true;
+        case 'lsqcurvefit'
 
-    % new form
-    fitfcnNew{1} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3)))));
-    fitfcnNew{2} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
-                                 p(4)*erfc(((Fd)-p(5))/(sqrt(2)*p(6))))); 
-    fitfcnNew{3} = @(p, Fd)(1/2*(p(1)*erfc(((Fd)-p(2))/(sqrt(2)*p(3))) + ...
-                                 p(4)*erfc(((Fd)-p(5))/(sqrt(2)*p(6))) + ...
-                                 p(7)*erfc(((Fd)-p(8))/(sqrt(2)*p(9))))); 
-
-    switch fitname
-        case "erf-old"
-            if Nmodes > 2
-                warning('This model not defined for more than 2 modes. Resetting to two modes.');
-                Nmodes = 2;
-            end
-            fiteq = fitfcn{Nmodes};
-            Nparams = 2*Nmodes + 1;
-        case "erf-new"
-            fiteq = fitfcnNew{Nmodes};
-            Nparams = 3*Nmodes;
+        case 'lsqnonlin'
+            opts.Display = 'Off';
+            opts.MaxFunEvals = 26000;
+            opts.MaxIter = 24000;
+%             opts.Weights = weights;
+            opts.TolFun = 1e-07;
+            opts.TolX = 1e-07;
+            opts.DiffMinChange = 1e-08;
+            opts.DiffMaxChange = 0.01;
+%             opts.StartPoint = startpoint;
+%             opts.Lower = lb;
+%             opts.Upper = ub;
+        case 'ga'
+        case 'particleswarm'
+            opts.HybridFcn = "fmincon";
+            opts.SwarmSize = swarm_sz;
+            opts.UseParallel = true;
+            opts.Display = 'final';
+        case 'patternsearch'            
+            opts.StepTolerance = 1e-5;
+            opts.MeshTolerance = 1e-5;
+            opts.Algorithm = 'nups-mads';
+            opts.Cache = "on";
+            opts.PlotFcn = {'psplotbestf'};
+            opts.UseParallel = true;
+            opts.Display = 'final';
         otherwise
-            error('Fit type not defined.');           
+            error('Fitting method type undefined.');            
     end
-
-
-    switch nargout
-        case 1
-            varargout{1} = fiteq;
-        case 2
-            varargout{1} = fiteq;
-            varargout{2} = Nparams;
-    end
-
 end
-
-
-
-
-
-% function opts = setup_fitoptions(weights, Nmodes, startpoint)
-% 
-%     opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-% 
-%     opts.Display = 'Off';
-%     opts.MaxFunEvals = 26000;
-%     opts.MaxIter = 24000;
-%     opts.Weights = weights;
-%     opts.TolFun = 1e-07;
-%     opts.TolX = 1e-07;
-%     opts.DiffMinChange = 1e-08;
-%     opts.DiffMaxChange = 0.01;
-% 
-%     % Lower and upper bounds for each parameter
-%     %    [a  am   as   bm   bs ]
-%     lb = [0 -Inf  0   -Inf  0  ];
-%     ub = [1  Inf  Inf  Inf  Inf];
-%     
-%     % Default starting points if none are available
-%     % p0 = [0.82582 0.07818 0.44268 0.10666 0.96190];
-%     p0 = [0.85 -1.5 0.5 0.75 1];
-% 
-%     k = Nmodes*2 + 1;
-% 
-%     opts.StartPoint = startpoint(1:k);
-%     opts.Lower = lb(1:k);
-%     opts.Upper = ub(1:k);
-% 
-% end
