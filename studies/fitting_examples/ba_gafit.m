d56df8300733b43e0a1ad568c520e110e5c766dc
Jeremy A. Cribb <jcribb@unc.edu>
2024-05-04 06:47:22 -0400

    Genetic algorithm study. Run #2- These settings have 'worked' twice in a row for a standard 3 merged wells of datapoints (a column on the plate). Still exits a fair amount by exceding number of generations. Will increase that next.

--- studies/fitting_examples/ba_gafit.m
+++ studies/fitting_examples/ba_gafit.m
@@ -1,15 +1,13 @@
-function ga_summary = ba_gafit(dftable)
+function ga_summary = ba_gafit(dftable, plotTF)
 
 % Global optimization fitting routine for Adhesion Assay using the
 % genetic algorithm. This algorithm serves as a good choice over 
 % the particleswarm because it allows for fitting with constraints. It is
 % painfully slow, though.
 %
-
-clear ps optimized_params error exitflag 
-clear ga_summary
-
-Data = dftable;
+if nargin < 2 || isempty(plotTF)
+    plotTF = false;
+end
 
 if ~exist('ga_summary', 'var')
     ga_summary = table('Size', [0 9], ...
@@ -17,14 +15,17 @@ if ~exist('ga_summary', 'var')
                        'VariableNames', {'PlateID', 'Run', 'MaxGenerations', 'PopulationSize', 'SolveTime', 'ExitFlag', 'OptimizedParameters', 'TotalError', 'RedChiSq'});
 end
 
+Data = dftable;
+
 Nmodes = 2;
 Nruns = 1;
 
 % Define "ga" optimization options that do not change during run
 options = ba_fitoptions("ga");
-options.FunctionTolerance = 1e-7;
-options.PlotFcn = {'gaplotscores','gaplotbestf'};
+options.FunctionTolerance = 1e-8;
+options.PlotFcn = {'gaplotscores','gaplotbestlogf'};
 options.UseParallel = true;
+
 
 % Some number (m) of plates to process
 for m = 1:height(Data)
@@ -35,14 +36,26 @@ for m = 1:height(Data)
     logforce_nN = log10(rawdata.Force);
     logforceinterval = log10(rawdata.ForceInterval);
     fractionLeft = rawdata.FractionLeft; 
-    weights = rawdata.Weights;
+%     weights = rawdata.Weights;
+    weights = ones(size(rawdata.Weights));
+
+    Np = numel(logforce_nN);
 
     % *** ga-specific options ***
-    max_generations = 1500;
+    max_generations = 3000;
+    popsize = floor(Np/2);
+    elitecount = ceil(popsize * 0.333);
+    if elitecount >= popsize, elitecount = popsize -1; end
     options.MaxGenerations = max_generations;
-    options.PopulationSize = floor(numel(logforce_nN)/2);
+    options.PopulationSize = popsize;   
+    options.ConstraintTolerance = 1e-4;
+    options.CrossoverFraction = 0.6;
+    options.EliteCount = elitecount; 
+    options.HybridFcn = 'fmincon';
 
-    fig = 1000+(100*Nmodes)+m;
+
+
+    fig{m} = figure;
 
     fout  = ba_fit_setup(Nmodes, weights);
 
@@ -57,9 +70,9 @@ for m = 1:height(Data)
         
         % Call the global optimizer [x,fval,exitflag,output,population,scores] 
         [optimized_params(k,:), error(k,1), exitflag(k,1)] = ga(@(p) objectiveFunction(p, fout.fcn, logforce_nN, fractionLeft, weights), ...
-                                        fout.Nparams, fout.Aeq, fout.beq, [], [], fout.lb, fout.ub, [], options);
+                                        fout.Nparams, fout.Aineq, fout.bineq, [], [], fout.lb, fout.ub, [], options);
         
-        rchisq(k,:) = red_chisquare(optimized_params(k,:), fout.fcn, logforce_nN, fractionLeft);
+        rchisq(k,:) = red_chisquare(optimized_params(k,:), fout.fcn, logforce_nN, fractionLeft, weights);
         
         t=toc;
         
@@ -69,13 +82,21 @@ for m = 1:height(Data)
         ga_summary = vertcat(ga_summary, tmpga);
         
 
-        
-       % figure(fig); 
-       % subplot(1,3,1); 
-       % hold on;
-       % plot(logforce_nN, fout.fcn(optimized_params(k,:), logforce_nN), 'r-'); 
-       % drawnow
-    
+    if plotTF                
+        figure(fig{m}); 
+        clf
+        hold on
+            plot( logforce_nN, fractionLeft, 'Color', [0.8 0.8 0.8], 'Marker', '.', 'LineStyle', 'none');
+            plot( logforceinterval(:,1), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');
+            plot( logforceinterval(:,2), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');      
+            plot(logforce_nN, fout.fcn(optimized_params(k,:), logforce_nN), 'r-'); 
+        hold off
+        xlabel('log_{10}(Force [nN])');
+        ylabel('Fraction left');
+        title(string(PlateID), 'Interpreter','none');
+        fig{m}.Units = 'normalized';
+        fig{m}.Position = [0.6161    0.5185    0.2917    0.3889];
+    end
     
     end
 
@@ -99,7 +120,7 @@ function error = objectiveFunction(params, fitfcn, logforce_nN, fractionLeft, we
     % Calculate weighted squared error
     weighted_errors = weights .* (model_predictions - fractionLeft).^2;
 %     weighted_errors = smooth(weighted_errors,5);
-    error = sum(weighted_errors);
+    error = sum(weighted_errors, [], 'omitnan');
 end
 
 
