9ff73cf27857885daf4102002d39a02c20e9bdb1
Jeremy A. Cribb <jcribb@unc.edu>
2024-05-05 07:50:05 -0400

    Changing default options for different fitting methods that can be used in the Adhesion Assay. These settings are specific to the analysis path here.

--- analysis/ba_fitoptions.m
+++ analysis/ba_fitoptions.m
@@ -8,17 +8,17 @@ function opts = ba_fitoptions(method)
 
     % Special case: when using the 'fit' function from the curve-fitting toolbox
     if strcmpi(method,'fit')
-        opts = fitoptions;
-        opts.Normalize = 'off';
-        opts.Disply = 'off';
-        opts.Algorithm = 'trust-region-reflective';
-        opts.Robust = 'off';
+        opts = fitoptions('Method', 'NonlinearLeastSquares');
+        opts.Normalize = 'Off';
+        opts.Display = 'Off';
+        opts.Algorithm = 'Trust-Region';
+        opts.Robust = 'Off';
         opts.MaxFunEvals = 26000;
         opts.MaxIter = 24000;
-        opts.TolFun = 1e-07;
-        opts.TolX = 1e-07;
-        opts.DiffMinChange = 1e-08;
-        opts.DiffMaxChange = 0.01;
+        opts.TolFun = 1e-08;
+        opts.TolX = 1e-08;
+        opts.DiffMinChange = 1e-09;
+        opts.DiffMaxChange = 0.001;
         return
     end
 
@@ -27,13 +27,16 @@ function opts = ba_fitoptions(method)
     opts = optimoptions(method);
 
     switch method
-        case 'fmincon'
+        case 'fmincon' % "con" here refers to "constrained"
+            % "trust-region-reflective" requires more info on the input or
+            % limits the ability to use constraints AND bounds.
             opts.Algorithm = 'interior-point';
             opts.UseParallel = true;
         case 'lsqcurvefit'
             opts.Display = 'Off'; % Display='final';
         case 'lsqnonlin'
             opts.Display = 'Off'; % Display='final';
+            opts.Algorithm = 'levenberg-marquardt';
             opts.MaxFunEvals = 26000;
             opts.MaxIter = 24000;
             opts.TolFun = 1e-07;
@@ -52,13 +55,15 @@ function opts = ba_fitoptions(method)
             opts.UseParallel = true;
             opts.Display = 'Off'; % Display='final';
         case 'patternsearch'            
-            opts.StepTolerance = 1e-5;
-            opts.MeshTolerance = 1e-5;
+            opts.InitialMeshSize = 0.1;
+            opts.StepTolerance = 1e-6;
+            opts.MeshTolerance = 1e-6;
             opts.Algorithm = 'nups-mads';
             opts.Cache = "on";
-            opts.PlotFcn = {'psplotbestf'};
+            opts.PlotFcn = {'psplotbestlogf'};
             opts.UseParallel = true;
             opts.Display = 'Off'; % Display='final';
+            
         otherwise
             error('Fitting method type undefined.');            
     end
