52d631137123ec2785644db3ae49935c933d02a9
Jeremy A. Cribb <jcribb@unc.edu>
2024-05-05 07:08:16 -0400

    Unclear what's causing the scaled and centered data not to fit as well. Returned to unscaled fits, rearranged the output table to offer better information, removed the weights, and tweaked the options structure settings, as well as explicitly defining the mutation funciton. Still unsure if the mutations function was already being used, but at least now there's a stub for tweaking mutation function parameters.

--- studies/fitting_examples/ba_gafit.m
+++ studies/fitting_examples/ba_gafit.m
@@ -12,7 +12,7 @@ end
 if ~exist('ga_summary', 'var')
     ga_summary = table('Size', [0 9], ...
                        'VariableTypes', {'categorical', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double'}, ...
-                       'VariableNames', {'PlateID', 'Run', 'MaxGenerations', 'PopulationSize', 'SolveTime', 'ExitFlag', 'OptimizedParameters', 'TotalError', 'RedChiSq'});
+                       'VariableNames', {'PlateID', 'SolveTime', 'OptimizedParameters', 'TotalError', 'RedChiSq', 'ExitFlag', 'GenerationSolveCount', 'MaxGenerations', 'PopulationSize'});
 end
 
 Data = dftable;
@@ -22,7 +22,6 @@ Nruns = 1;
 
 % Define "ga" optimization options that do not change during run
 options = ba_fitoptions("ga");
-options.FunctionTolerance = 1e-8;
 options.PlotFcn = {'gaplotscores','gaplotbestlogf'};
 options.UseParallel = true;
 
@@ -36,38 +35,41 @@ for m = 1:height(Data)
     logforce_nN = log10(rawdata.Force);
     logforceinterval = log10(rawdata.ForceInterval);
     fractionLeft = rawdata.FractionLeft; 
-    weights = rawdata.Weights;
-%     weights = ones(size(rawdata.Weights));
+%     weights = rawdata.Weights;
+    weights = ones(size(rawdata.Weights));
 
-    Np = numel(logforce_nN);
+    Ns = numel(logforce_nN); % Ns = number of samples
 
     % *** ga-specific options ***
-    max_generations = 4000;
-    popsize = floor(Np/2);
-    elitecount = ceil(popsize * 0.333);
-    if elitecount >= popsize, elitecount = popsize -1; end
-    if sum(weights) == numel(weights), FuncTol = 1e-7; else FuncTol = 2e-9; end
-    
+    max_generations = 6000;
+    popsize = floor(Ns/2);
+    elitecount = ceil(popsize * 0.28);
+
+    if elitecount >= popsize, elitecount = popsize-1; end
+    if sum(weights) == numel(weights), FuncTol = 3e-8; else FuncTol = 2e-10; end
+
     options.MaxGenerations = max_generations;
     options.FunctionTolerance = FuncTol;
+    options.MutationFcn = @mutationadaptfeasible;
     options.PopulationSize = popsize;   
     options.ConstraintTolerance = 1e-4;
-    options.CrossoverFraction = 0.6;
+    options.CrossoverFraction = 0.49;
     options.EliteCount = elitecount; 
     options.HybridFcn = 'fmincon';
 
-
-
     fig{m} = figure;
     xlabel('log_{10}(Force [nN])');
-    ylabel('Fraction left');
+    ylabel('Fraction Left');
     title(string(PlateID), 'Interpreter','none');
     title(join(string(Data{m,{'PlateID', 'BeadChemistry', 'SubstrateChemistry', 'Media', 'pH'}}), ', '), 'Interpreter','none');
     fig{m}.Units = 'normalized';
-    fig{m}.Position = [0.6161    0.5185    0.2917    0.3889];
+    fig{m}.Position = [0.616 0.519 0.292 0.389];
 
 
-    fout  = ba_fit_setup(Nmodes, weights);
+    fout  = ba_fit_setup(Nmodes);
+
+%     [zlogforce_nN, mu, sigma] = zscore(logforce_nN(:));
+    
 
     % Some number (k) of subsampling fits to generate. This could also
     % be changed to monitor changes in algorithm style parameter sweeps,
@@ -79,28 +81,30 @@ for m = 1:height(Data)
         tic 
         
         % Call the global optimizer [x,fval,exitflag,output,population,scores] 
-        [optimized_params(k,:), error(k,1), exitflag(k,1)] = ga(@(p) objectiveFunction(p, fout.fcn, logforce_nN, fractionLeft, weights), ...
+        [optimized_params(k,:), error(k,1), exitflag(k,1), output(k,1)] = ga(@(p) objectiveFunction(p, fout.fcn, logforce_nN, fractionLeft, weights), ...
                                         fout.Nparams, fout.Aineq, fout.bineq, [], [], fout.lb, fout.ub, [], options);
+
+%         optimized_params(k,:) = zoptimized_params(k,:) * sigma + mu;
         
         rchisq(k,:) = red_chisquare(optimized_params(k,:), fout.fcn, logforce_nN, fractionLeft, weights);
         
         t=toc;
         
-        tmpga = table(PlateID, k, max_generations, floor(numel(logforce_nN)/2), t, exitflag(k,1), optimized_params(k,:), error(k,1), rchisq(k,:), ...
-                     'VariableNames', {'PlateID', 'Run', 'MaxGenerations', 'PopulationSize', 'SolveTime', 'ExitFlag', 'OptimizedParameters', 'TotalError', 'RedChiSq'});
+        tmpga = table(PlateID, t, optimized_params(k,:), error(k,1), rchisq(k,:), exitflag(k,1), output.generations, max_generations, popsize, ...
+                     'VariableNames', {'PlateID', 'SolveTime', 'OptimizedParameters', 'TotalError', 'RedChiSq', 'ExitFlag', 'GenerationSolveCount', 'MaxGenerations', 'PopulationSize'});
         
         ga_summary = vertcat(ga_summary, tmpga);
         
 
-    if plotTF                
-        figure(fig{m}); 
-        hold on
-            plot( logforce_nN, fractionLeft, 'Color', [0.8 0.8 0.8], 'Marker', '.', 'LineStyle', 'none');
-            plot( logforceinterval(:,1), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');
-            plot( logforceinterval(:,2), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');      
-            plot(logforce_nN, fout.fcn(optimized_params(k,:), logforce_nN), 'r-'); 
-        hold off
-    end
+        if plotTF                
+            figure(fig{m}); 
+            hold on
+                plot( logforce_nN, fractionLeft, 'Color', [0.8 0.8 0.8], 'Marker', '.', 'LineStyle', 'none');
+                plot( logforceinterval(:,1), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');
+                plot( logforceinterval(:,2), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');      
+                plot(logforce_nN, fout.fcn(optimized_params(k,:), logforce_nN), 'r-'); 
+            hold off
+        end
     
     end
 
