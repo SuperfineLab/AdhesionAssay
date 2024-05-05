8b925e4a54b7cbcb2c8963385216a2c6a561c5ee
Jeremy A. Cribb <jcribb@unc.edu>
2024-05-05 07:34:13 -0400

    Fixed bug in this computation. Switched to matix multiply version outlined in reference cited in the code's comments.

--- analysis/red_chisquare.m
+++ analysis/red_chisquare.m
@@ -1,14 +1,25 @@
-function redcs = red_chisquare(params, fitfcn, logforce_nN, fractionLeft)
+function redchisq = red_chisquare(params, fitfcn, logforce_nN, fractionLeft, weights)
 % XXX @jeremy TODO: document this function
+% Using the weighted-reduced chi-square equation from: 
+% https://en.wikipedia.org/wiki/Reduced_chi-squared_statistic
+% 
+% reduced_chi_square = r'*W*r/nu, where r is the residuals from the fit, W
+% is the weight matrix, and nu is the degrees of freedom (Ndatapoints - Nparams)
+%
+
+    if nargin < 5 || isempty(weights)
+        weights(1,:) = ones(numel(logforce_nN),1);
+    end
 
     % Calculate model predictions using params and xdata
     model_predictions = fitfcn(params, logforce_nN);
 
-    % Calculate weighted squared error
-    chisquare = sum( (model_predictions - fractionLeft).^2 ./  fractionLeft);
-
+    % residuals, R, from model prediction
+    R(:,1) = model_predictions - fractionLeft;
+    W = diag(weights);
     dof = numel(logforce_nN) - numel(params);
 
-    redcs = chisquare / dof;
+    redchisq = (R' * W * R)/dof;
+
 end
 
