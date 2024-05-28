0c234cda738d6067f7a88ef3e164ee2be2f03979
Jeremy A. Cribb <jcribb@unc.edu>
2024-05-28 12:33:29 -0400

    Greatly changed to accommodate actual analytical workflow once the genetic algorithm was selected.

--- studies/fitting_examples/ba_gafit.m
+++ studies/fitting_examples/ba_gafit.m
@@ -1,4 +1,4 @@
-function ga_summary = ba_gafit(dftable, plotTF)
+function [DataOut, ga_summary] = ba_gafit(dftable, plotTF)
 
 % Global optimization fitting routine for Adhesion Assay using the
 % genetic algorithm. This algorithm serves as a good choice over 
@@ -10,107 +10,113 @@ if nargin < 2 || isempty(plotTF)
 end
 
 if ~exist('ga_summary', 'var')
-    ga_summary = table('Size', [0 9], ...
-                       'VariableTypes', {'categorical', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double'}, ...
-                       'VariableNames', {'PlateID', 'SolveTime', 'OptimizedParameters', 'TotalError', 'RedChiSq', 'ExitFlag', 'GenerationSolveCount', 'MaxGenerations', 'PopulationSize'});
+    ga_summary = table('Size', [0 11], ...
+                       'VariableTypes', {'categorical', 'double', 'double', 'double', ...
+                                         'double', 'double', 'double', 'double', 'double', ...
+                                         'double', 'double'}, ...
+                       'VariableNames', {'PlateID', 'SolveTime', 'OptimizedParameters', 'TotalError', ...
+                                         'RedChiSq', 'ExitFlag', 'GenerationSolveCount', 'MaxGenerations', 'PopulationSize', ...
+                                         'FinalPop','FinalScore'});
 end
 
-Data = dftable;
+DataOut = dftable;
 
 Nmodes = 2;
-Nruns = 1;
+
+% for plotting
+stdclr = lines(7);
 
 % Define "ga" optimization options that do not change during run
-options = ba_fitoptions("ga");
-options.PlotFcn = {'gaplotscores','gaplotbestlogf'};
-options.UseParallel = true;
-
+% ga_opts = ba_fitoptions("ga");
+isTableCol = @(t, thisCol) ismember(thisCol, t.Properties.VariableNames);
 
 % Some number (m) of plates to process
-for m = 1:height(Data)
+for m = 1:height(DataOut)
 
-    PlateID = Data.PlateID(m);
-    rawdata = Data.RawData{m};
+    ga_opts = dftable.gaOpts(m);
+    if plotTF
+        ga_opts.PlotFcn = {'gaplotscores','gaplotbestlogf'};
+    end
+
+    PlateID = DataOut.PlateID(m);
+    rawdata = DataOut.RawData{m};
     
     logforce_nN = log10(rawdata.Force);
     logforceinterval = log10(rawdata.ForceInterval);
     fractionLeft = rawdata.FractionLeft; 
-%     weights = rawdata.Weights;
-    weights = ones(size(rawdata.Weights));
+    weights = rawdata.Weights; %     weights = ba_weights(rawdata.ForceInterval, 0.95, 'quantile', 4);
 
     Ns = numel(logforce_nN); % Ns = number of samples
 
     % *** ga-specific options ***
-    max_generations = 6000;
-    popsize = floor(Ns/2);
-    elitecount = ceil(popsize * 0.28);
-
-    if elitecount >= popsize, elitecount = popsize-1; end
-    if sum(weights) == numel(weights), FuncTol = 3e-8; else FuncTol = 2e-10; end
-
-    options.MaxGenerations = max_generations;
-    options.FunctionTolerance = FuncTol;
-    options.MutationFcn = @mutationadaptfeasible;
-    options.PopulationSize = popsize;   
-    options.ConstraintTolerance = 1e-4;
-    options.CrossoverFraction = 0.49;
-    options.EliteCount = elitecount; 
-    options.HybridFcn = 'fmincon';
-
-    fig{m} = figure;
-    xlabel('log_{10}(Force [nN])');
-    ylabel('Fraction Left');
-    title(string(PlateID), 'Interpreter','none');
-    title(join(string(Data{m,{'PlateID', 'BeadChemistry', 'SubstrateChemistry', 'Media', 'pH'}}), ', '), 'Interpreter','none');
-    fig{m}.Units = 'normalized';
-    fig{m}.Position = [0.616 0.519 0.292 0.389];
-
+%     popsize = floor(Ns/2);
+%     elitecount = ceil(popsize * 0.28);
+%     if elitecount >= popsize, elitecount = popsize-1; end
+%     if sum(weights) == numel(weights), ga_opts.FunctionTolerance = 3e-8; end    
+%     ga_opts.PopulationSize = popsize;   
+%     ga_opts.EliteCount = elitecount; 
+%     ga_opts.MaxGenerations = 400;
+    if isTableCol(dftable, 'FinalPop') && ~isempty(dftable.FinalPop) 
+        ga_opts.InitialPopulationMatrix = dftable.FinalPop{m};
+    end
 
     fout  = ba_fit_setup(Nmodes);
 
-%     [zlogforce_nN, mu, sigma] = zscore(logforce_nN(:));
-    
+    % Call the global optimizer [x,fval,exitflag,output,population,scores] 
+    tic 
+    [optimized_params, error, exitflag, output, finalpop, finalscore] = ga(@(p) objectiveFunction(p, fout.fcn, logforce_nN, fractionLeft, weights), ...
+                                    fout.Nparams, fout.Aineq, fout.bineq, [], [], fout.lb, fout.ub, [], ga_opts);
+    t=toc;
 
-    % Some number (k) of subsampling fits to generate. This could also
-    % be changed to monitor changes in algorithm style parameter sweeps,
-    % e.g., swarm-size for particleswarms.
-    for k = 1:Nruns
+    rchisq = red_chisquare(optimized_params, fout.fcn, logforce_nN, fractionLeft, weights);
     
-        % If changing options for parameter search, do it here
-        
-        tic 
-        
-        % Call the global optimizer [x,fval,exitflag,output,population,scores] 
-        [optimized_params(k,:), error(k,1), exitflag(k,1), output(k,1)] = ga(@(p) objectiveFunction(p, fout.fcn, logforce_nN, fractionLeft, weights), ...
-                                        fout.Nparams, fout.Aineq, fout.bineq, [], [], fout.lb, fout.ub, [], options);
+    % Set up the output diagnostic/summary table for the run
+    ga_summary = table(PlateID, t, {optimized_params}, error, rchisq, exitflag, ...
+                       ga_opts.MaxGenerations, ga_opts.PopulationSize, ...
+                       output.generations, {finalpop}, {finalscore}, ga_opts, ...
+                       'VariableNames', {'PlateID', 'SolveTime', 'OptimizedParameters', 'TotalError', 'RedChiSq', 'ExitFlag', ...
+                                         'MaxGenerations', 'PopulationSize', ...
+                                         'GenerationSolveCount', 'FinalPop', 'FinalScore', 'gaOpts'});
+      
+    cftbx_opts = ba_fitoptions('fit');    
+    [StatT, BootstatT] = ba_bootstrap_fit(logforce_nN, fractionLeft, weights, fout, cftbx_opts);
 
-%         optimized_params(k,:) = zoptimized_params(k,:) * sigma + mu;
-        
-        rchisq(k,:) = red_chisquare(optimized_params(k,:), fout.fcn, logforce_nN, fractionLeft, weights);
-        
-        t=toc;
-        
-        tmpga = table(PlateID, t, optimized_params(k,:), error(k,1), rchisq(k,:), exitflag(k,1), output.generations, max_generations, popsize, ...
-                     'VariableNames', {'PlateID', 'SolveTime', 'OptimizedParameters', 'TotalError', 'RedChiSq', 'ExitFlag', 'GenerationSolveCount', 'MaxGenerations', 'PopulationSize'});
-        
-        ga_summary = vertcat(ga_summary, tmpga);
-        
+    DataOut.FitParams(m) = {StatT.p};
+    DataOut.confFitParams(m) = {StatT.pconf};
+    DataOut.sse(m) = StatT.sse;
+    DataOut.rsquare(m) = StatT.rsquare;
+    DataOut.adjrsquare(m) = StatT.adjrsquare;
+    DataOut.dfe(m) = StatT.dfe;
+    DataOut.rmse(m) = StatT.rmse;
+    DataOut.redchisq(m) = StatT.redchisq;
+    DataOut.FitSetup(m) = fout;
+    DataOut.FitOptions(m) = cftbx_opts;
+    DataOut.BootstatT{m} = BootstatT;
+    DataOut.FinalPop{m} = finalpop;
+    DataOut.FinalScore{m} = finalscore;
+    DataOut.gaOpts(m) = ga_opts;
 
-        if plotTF                
-            figure(fig{m}); 
-            hold on
-                plot( logforce_nN, fractionLeft, 'Color', [0.8 0.8 0.8], 'Marker', '.', 'LineStyle', 'none');
-                plot( logforceinterval(:,1), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');
-                plot( logforceinterval(:,2), fractionLeft, 'Color', [0.8 0.8 0.8], 'LineStyle', '-');      
-                plot(logforce_nN, fout.fcn(optimized_params(k,:), logforce_nN), 'r-'); 
-            hold off
-        end
-    
+    % Assign new parameters to the (modified and then ultimately outputted) data table
+
+    if plotTF                
+        fig{m} = figure;
+        hold on
+           plot( logforce_nN, fractionLeft, 'Marker','o', 'MarkerSize', 3, 'MarkerEdgeColor',stdclr(1,:), 'MarkerFaceColor',stdclr(1,:), 'LineStyle','none');
+           plot( logforceinterval(:,1), fractionLeft, 'Marker','>', 'MarkerSize', 2, 'MarkerEdgeColor',stdclr(1,:)*1.25, 'MarkerFaceColor','none', 'LineStyle','none');
+           plot( logforceinterval(:,2), fractionLeft, 'Marker','<', 'MarkerSize', 2, 'MarkerEdgeColor',stdclr(1,:)*1.25, 'MarkerFaceColor','none', 'LineStyle','none');
+           plot( logforce_nN, fout.fcn(optimized_params, logforce_nN), 'k-', 'LineWidth', 1); 
+        hold off        
+        xlabel('log_{10}(Force [nN])');
+        ylabel('Fraction Left');
+        title(string(PlateID), 'Interpreter','none');
+        title(join(string(DataOut{m,{'PlateID', 'BeadChemistry', 'SubstrateChemistry', 'Media', 'pH'}}), ', '), 'Interpreter','none');
+        fig{m}.Units = 'normalized';
+        fig{m}.Position = [0.616 0.519 0.292 0.389];
+        legend(['data', 'error', 'error', compose('%02.6d',error(:)')]);    
     end
 
-   % legend(['data', 'error', 'error', compose('%02.6d',error(:)')]);
-
 end % end for-loop
+
 
 end % end function
 
