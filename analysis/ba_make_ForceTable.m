9351a618cd18c2ae2587066ca5619c1e1e0f3068
Jeremy A. Cribb <jcribb@unc.edu>
2024-05-28 12:10:34 -0400

    Added documentation for the function and added weightstyle as an input.

--- analysis/ba_make_ForceTable.m
+++ analysis/ba_make_ForceTable.m
@@ -1,4 +1,19 @@
-function ForceTable = ba_make_ForceTable(Data)
+function ForceTable = ba_make_ForceTable(Data, weightstyle)
+% BA_MAKE_FORCETABLE computes bead-level forces based on z-velocity
+%
+%   ForceTable = ba_make_ForceTable(Data, weightstyle)
+%
+% Outputs:
+%    ForceTable - standard Table output for Adhesion Assay bead forces 
+%
+% Inputs:
+%    Data - standard Table outputted from ba_process_expt/ba_load_raw_data
+%    weightstyle - 
+%
+
+    if nargin < 2 || isempty(weightstyle)
+        weightstyle = 'unweighted';
+    end
 
     FileTable = Data.FileTable;
     
@@ -12,7 +27,7 @@ function ForceTable = ba_make_ForceTable(Data)
        myZtable = Data.TimeHeightVidStatsTable(Data.TimeHeightVidStatsTable.Fid == Fid,:);      
        myTracking = Data.TrackingTable(Data.TrackingTable.Fid == Fid, :);
     
-       ForceTable{k} = ba_get_linefits(myTracking, calibum, visc_Pas, bead_diameter_um, Fid);  
+       ForceTable{k} = ba_get_linefits(myTracking, calibum, visc_Pas, bead_diameter_um, Fid, weightstyle);  
        ForceTable{k}.ZmotorPos = interp1(myZtable.Time, myZtable.ZHeight, ForceTable{k}.Mean_time);
        ForceTable{k}.Properties.VariableUnits{'ZmotorPos'} = '[mm]';
        ForceTable{k}.Properties.VariableDescriptions{'ZmotorPos'} = 'Magnet z-distance from substrate in mm';
