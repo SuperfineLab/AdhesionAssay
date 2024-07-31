56e9098cbb66b581b25f37e0b69a873409f9986f
Jeremy A. Cribb <jcribb@unc.edu>
2024-07-31 14:28:24 -0400

    Updated to new datatypes.

--- util/ba_combine_runs.m
+++ util/ba_combine_runs.m
@@ -3,11 +3,17 @@ function DataOut = ba_combine_runs(DataInA, DataInB)
 %
 
 repeatFID = intersect(DataInA.FileTable.Fid, DataInB.FileTable.Fid);
+repeatPlateID = intersect(DataInA.FileTable.PlateID, DataInB.FileTable.PlateID);
 
-if isempty(repeatFID)
+if isempty(repeatFID) && isempty(repeatPlateID)
     DataOut.FileTable = [DataInA.FileTable; DataInB.FileTable];
-    DataOut.BeadForceTable = [DataInA.BeadForceTable; DataInB.BeadForceTable];
+    DataOut.TimeHeightVidStatsTable = [DataInA.TimeHeightVidStatsTable; DataInB.TimeHeightVidStatsTable];
     DataOut.BeadInfoTable = [DataInA.BeadInfoTable; DataInB.BeadInfoTable];
+    DataOut.TrackingTable = [DataInA.TrackingTable; DataInB.TrackingTable];
+    DataOut.BeadForceTable = [DataInA.BeadForceTable; DataInB.BeadForceTable];
+    DataOut.OptimizedStartTable = [DataInA.OptimizedStartTable; DataInB.OptimizedStartTable];
+    DataOut.ForceFitTable = [DataInA.ForceFitTable; DataInB.ForceFitTable];
+    DataOut.DetachForceTable = [DataInA.DetachForceTable; DataInB.DetachForceTable];
 else
     error('Repeat FID. Likely the data are already combined. Data cannot be further combined.');
 end
