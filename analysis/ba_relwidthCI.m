afb1522c68f23ad40424297e28b38d258604cc49
Jeremy A. Cribb <jcribb@unc.edu>
2024-08-03 09:04:47 -0400

    Updated function to work for both column and row-oriended input data.

--- analysis/ba_relwidthCI.m
+++ analysis/ba_relwidthCI.m
@@ -4,6 +4,16 @@ function outs = ba_relwidthCI(val, conf_interval)
 % measured value over which the confidence interval is defined.
 %
 
-    outs = abs(diff(conf_interval,[],2) ./ val); 
+    [Nrows, Ncols] = size(val);
+
+    if Nrows == 1 % column-ordered
+        DIFFDIM = 1;
+    elseif Ncols == 1 % row-ordered
+        DIFFDIM = 2;
+    else
+        error('Not set up for larger than 2d matrices. Expand the function.');
+    end
+    
+    outs = abs(diff(conf_interval,[],DIFFDIM) ./ val); 
 
 end
