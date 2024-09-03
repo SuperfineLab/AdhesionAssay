22a605ac2180e1d57ab48e5d12a2e5dc4d6db2e3
Jeremy A. Cribb <jcribb@unc.edu>
2024-09-03 13:05:55 -0400

    Refactored analysis of adhesion assay controls.

--- studies/controls_2024paper.m
+++ studies/controls_2024paper.m
@@ -13,16 +13,20 @@ addpath(genpath([path_for_genpath, filesep, 'AdhesionAssay']));
 % close all
 
 rootdir = pwd;
+mylinecolor = lines(7);
 
-improveBadFitsTF = true;
-savedatafilesTF = true;
+improveBadFitsTF = false;
+savedatafilesTF = false;
 % recalculateTF = true;
 
 % grouping variable for the STUDY (not when loading the data)
-groupvars = {'SubstrateChemistry', 'BeadChemistry', 'Media', 'pH'};
+% groupvars = {'PlateID', 'PlateColumn','SubstrateChemistry', 'BeadChemistry', 'Media', 'pH'};
+groupvars = {'PlateColumn', 'BeadChemistry', 'SubstrateChemistry'};
 
 % Loading the data and computing the plate-level stats and force curves
-% requires that "PlateID" be a "grouping variable." After that, the
+% requires that "PlateID" (and possibly 'PlateColumn') be 
+% a "grouping variable."
+% loaddata_groupvars = unique(['PlateID', 'PlateColumn', groupvars], 'stable');
 loaddata_groupvars = unique(['PlateID', groupvars], 'stable');
 
 DataSetDirs = get_dataset_list;
@@ -34,129 +38,100 @@ else
     adp = 'K:\expts\AdhesionAssay\datasets_NOTvideo\';
 end
 
-% Load the data sources (one source per plate), attach the PlateID, and then 
-% concatenate into one big table.
-%
-% ** The load_bigstudy_data function is at the very bottom of this file. **
-if ~exist('Broot', 'var')
+% check for already cleaned datafile
+cleanfile = dir('K:\expts\AdhesionAssay\AdhesionAssay_2024ControlStudy\2024.08.26_cerium_Broot_CONTROLstudy_COLUMNwise_sampling_manually-adjusted-mode-count.UIedited.mat');
+if ~isempty(cleanfile)
+    cleandata = load(cleanfile.name);
+    ForceFitTable = cleandata.ForceFitTable;
+    DetachForceTable = ba_decouple_modes(ForceFitTable, groupvars);
+elseif ~exist('Broot', 'var')
+    % Load the data sources (one source per plate), attach the PlateID, and then 
+    % concatenate into one big table.
+    %
+    % ** The load_bigstudy_data function is at the very bottom of this file. **
     Broot = load_bigstudy_data(adp, DataSetDirs, loaddata_groupvars, improveBadFitsTF, savedatafilesTF );   
-end
 
-% Improve poorly-discovered force curve fits, if necessary
-if improveBadFitsTF
-    [Broot.ForceFitTable, ...
-     Broot.OptimizedStartTable] = ba_improve_bad_fits(Broot.ForceFitTable, ...
-                                                      Broot.OptimizedStartTable, ...
-                                                      loaddata_groupvars);    
-end
-
-if ~isfield(Broot,'DetachForceTable')
-    Broot.DetachForceTable = ba_decouple_modes(Broot.ForceFitTable, loaddata_groupvars);
-end
-B = clean_bigstudy_data(Broot);
-
-
-%
-% % Clean out the PWM and SNA
-%
-BeadChemsToKeep = {'COOH', 'PEG', 'HBE'};
-idxFile = ismember(B.FileTable.BeadChemistry, BeadChemsToKeep);
-
-FiltData.FileTable = B.FileTable(idxFile,:);
-FiltData.FileTable.BeadChemistry = removecats(FiltData.FileTable.BeadChemistry);
-FiltData.FileTable.BeadChemistry = reordercats(FiltData.FileTable.BeadChemistry, BeadChemsToKeep);
-
-FidToKeep = FiltData.FileTable.Fid;
-
-idxTime     = ismember(B.TimeHeightVidStatsTable.Fid, FidToKeep);
-idxBead     = ismember(B.BeadInfoTable.Fid, FidToKeep);
-idxTracking = ismember(B.TrackingTable.Fid, FidToKeep);
-idxForce    = ismember(B.BeadForceTable.Fid, FidToKeep);
-idxDetach   = ismember(B.ForceFitTable.BeadChemistry, BeadChemsToKeep);
-
-FiltData.TimeHeightVidStatsTable = B.TimeHeightVidStatsTable(idxTime,:);
-FiltData.BeadInfoTable = B.BeadInfoTable(idxBead,:);
-FiltData.TrackingTable = B.TrackingTable(idxTracking,:);
-FiltData.BeadForceTable    = B.BeadForceTable(idxForce,:);
-FiltData.ForceFitTable = B.ForceFitTable(idxDetach,:);
-FiltData.ForceFitTable.BeadChemistry = removecats(FiltData.ForceFitTable.BeadChemistry);
-
-OrigData = B;
-B = FiltData;
-
-plateNames(:,1) = unique(B.FileTable.PlateID);
-
-% Calculate basic statistics on the repeat measurements within a plate. 
-% The assay protocol for a plate runs each condition in triplicate. This 
-% aggregates each set of replicates into a single dataset/sample in each 
-% plate for each test condition.
-[g, PlateStatsT] = findgroups(B.FileTable(:,['PlateID', groupvars]));
-PlateStatsT.Nvideos    = splitapply(@numel, B.FileTable.Fid, g);
-PlateStatsT.FirstBeads = splitapply(@sum, B.FileTable.FirstFrameBeadCount, g);
-PlateStatsT.LastBeads  = splitapply(@sum, B.FileTable.LastFrameBeadCount, g);
-PlateStatsT.StuckPercent = PlateStatsT.LastBeads ./ PlateStatsT.FirstBeads * 100;        
-PlateStatsT.VisCol = grp2idx(PlateStatsT.BeadChemistry);        
-PlateStatsT.VisRow = grp2idx(PlateStatsT.PlateID);
-% PlateStatsT.VisRow = grp2idx(reordercats(PlateStatsT.PlateID, plateNames'));
-
-% This section just creates a set of xaxis and yaxis labels consistent with
-% categorical values within the constructed tables. In other words, here,
-% we want to pull out whatever bead chemistries and Plate IDs are represented 
-% and put them in the "right" places on the xaxis and yaxis, respectively. 
-[~, gxT] = findgroups(PlateStatsT(:,{'BeadChemistry'}));
-xstrings = string(table2array(gxT));
-[~, gyT] = findgroups(PlateStatsT(:,groupvars));
-% gyT.PlateID = reordercats(gyT.PlateID, plateNames');
-ystrings = string(plateNames);
-
-
-% % % ForceFitVars = {'PlateID',  'BeadChemistry', 'SubstrateChemistry', 'Media', ...
-% % %               'pH', 'DetachForce', 'relwidthDetachForce'};
-% % % 
-% % % Forces = innerjoin(B.ForceFitTable(:,ForceFitVars), PlateStatsT, 'Keys', ['PlateID' groupvars]);
-% % % 
-% % % 
-% % % SummaryDataT = innerjoin(Forces, PlateStatsT);
-% % % sz = [max(SummaryDataT.VisRow), max(SummaryDataT.VisCol)];
-% % % fmat = OrganizeSurfaceData(sz, SummaryDataT.VisRow, SummaryDataT.VisCol, abs(SummaryDataT.DetachForce));
-% % % fmat(isnan(fmat)) = 0;
-% % % plot_FuncSurface(fmat, pinefresh(255), 'Pulloff Force [nN]', xstrings, ystrings);
-% % % clim([0 15]);
-
-
-
-
-% %
-%   Plotting Functions
-% %
-function plot_FuncSurface(data, clrmap, titlestring, xstrings, ystrings)
-    if nargin < 5 || isempty(ystrings)
-        ystrings = 1:size(data,1);
+    % Improve poorly-discovered force curve fits, if necessary
+    if improveBadFitsTF
+        [Broot.ForceFitTable, ...
+         Broot.OptimizedStartTable] = ba_improve_bad_fits(Broot.ForceFitTable, ...
+                                                          Broot.OptimizedStartTable, ...
+                                                          loaddata_groupvars);    
     end
     
-    if nargin < 4 || isempty(xstrings)
-        xstrings = 1:size(data,2);
+    if ~isfield(Broot,'DetachForceTable')
+        Broot.DetachForceTable = ba_decouple_modes(Broot.ForceFitTable, loaddata_groupvars);
     end
+    B = clean_bigstudy_data(Broot);    
+end
 
-    figure;
-    imagesc(abs(data));
-    xlabel('Bead Functionalization');
-    ylabel('Substrate Funct -- Media');
-    title(titlestring);
-    colorbar;
-    colormap(clrmap)
+
+DetachForceTable.BeadChemistry = categorical(DetachForceTable.BeadChemistry, ["COOH", "PEG", "WGA", "HBE"], 'Ordinal', true);
+DetachForceTable.SubstrateChemistry = categorical(DetachForceTable.SubstrateChemistry, ["COOH", "PEG", "HBE"], 'Ordinal', true);
+DetachForceTable = sortrows(DetachForceTable, {'SubstrateChemistry','BeadChemistry'}, {'ascend','ascend'});
+
+AttachedForceThreshold = -0.65; % mean of two COOH ModeForces
+DetachForceTable.AttachedTF = (DetachForceTable.ModeForce > AttachedForceThreshold);
+detachgroupvars = [groupvars, 'AttachedTF'];
+
+ScaleThreshold = 0.25;
+LowScaleTF = (DetachForceTable.ModeScale <= ScaleThreshold);
+DetachForceTable(LowScaleTF,:) = [];
+
+% [gd, gdT] = findgroups(DetachForceTable(:,{'BeadChemistry', 'SubstrateChemistry'}));
+% factorNames = join(string(table2cell(gdT)),':');
+
+% [gd, gdT] = findgroups(DetachForceTable(:,{'BeadChemistry', 'SubstrateChemistry', 'AttachedTF'}));
+% factorNames = join(string(table2cell(gdT)),':');
+
+% [gd, gdT] = findgroups(DetachForceTable(:,{'PlateID'}));
+% factorNames = string(table2cell(gdT));
+
+[gd, gdT] = findgroups(DetachForceTable(:,{'PlateID', 'AttachedTF'}));
+factorNames= join(string(table2cell(gdT)));
+
+f = figure;
+hold on
+boxchart(gd, DetachForceTable.ModeForce, 'Notch','off', 'BoxFaceColor', mylinecolor(2,:));
+swarmchart(gd, DetachForceTable.ModeForce, 12, [0.3 0.3 0.3], 'filled');
+ax = gca;
+ax.XTick = [1:numel(factorNames)]';
+ax.XTickLabel = factorNames;
+ax.TickLabelInterpreter = 'none';
+ax.YLim = [-1.5 1.5];
+grid
+
+% [ag, agT] = findgroups(niT(:,{'SubstrateChemistry', 'BeadChemistry'}));
+% agT_groupNames = join(fliplr(string(table2cell(agT))),':');
+% agNames = agT_groupNames(ag);
+
+
+%% Bartlett test for equal variances. If sample-data fails this (p<0.05),
+%  then the standard anova1 (manova?) does not work. 
+bartTest = vartestn(DetachForceTable.ModeForce, factorNames(gd)) 
+if bartTest > 0.05
+    logentry('Equal variances test has passed. Standard ANOVA applies.');
+    %% Multi-variate anova (manova)
+    maov = manova(factorNames(gd), DetachForceTable.ModeForce);
+    m = multcompare(maov, 'CriticalValueType', 'bonferroni');
+    disp(m)
+    add_sigstar(m, f)
+
+else
+    % Move on to Kruskel-Wallis test
+    logentry('Variances between groups are not equal. Will use Kruskal-Wallis.');
+    [kw.p, kw.tbl, kw.stats] = kruskalwallis(DetachForceTable.ModeForce, factorNames(gd));
+    
+    figure; 
+    [mc.c, mc.m, mc.h, mc.gnames] = multcompare(kw.stats);
     ax = gca;
-    ax.YTick = 1:numel(ystrings);
-    ax.YTickLabel = ystrings;
-    ax.FontSize = 13;
-    ax.XTickLabel = xstrings;
     ax.TickLabelInterpreter = 'none';
-    ax.TickLength = [0 0];
-    drawnow;
+    % ax.Xlabel.Interpreter = 'none';
 end
 
 
 
+%% external functions
 function DataSetDirs = get_dataset_list
     DataSetDirs = { ...
                     '2024.01.25__COOHslide_COOHbeads_noint'; ...
@@ -165,4 +140,20 @@ function DataSetDirs = get_dataset_list
                     '2024.01.29__HBEslide_HBEbeads_noint'; ...
                     '2024.01.30__HBEslide2_HBEbeads_noint'; ...
                   };
+end
+
+
+function out = add_sigstar(m, fig)
+
+    m = m(m.pValue < 0.05, :);
+
+    if ~isempty(m)
+        ComparedNames = table2cell(m(:,{'Group1','Group2'}));
+        ComparedNames = cellfun( @(x1,x2){char(x1),char(x2)}, ComparedNames(:,1), ComparedNames(:,2), 'UniformOutput', false);
+    
+    
+        figure(fig);
+        sigstar(ComparedNames, m.pValue);
+    end
+    out = 0;
 end
